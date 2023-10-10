4273588e96690a8b2d2ee5ba0054e0c6f952da24
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-11 01:07:14 +0200

    chore: add unit tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ pkg/version/version_test.go
@@ -0,0 +1,61 @@
+package version
+
+import (
+	"runtime/debug"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestVersion(t *testing.T) {
+	require.NotEqual(t, "---", Version())
+}
+
+func TestBuildVersion(t *testing.T) {
+	BuildVersion = "test"
+	require.Equal(t, "test", Version())
+}
+
+func TestTime(t *testing.T) {
+	require.Equal(t, "---", Time())
+}
+
+func TestHash(t *testing.T) {
+	require.Equal(t, "---", Hash())
+}
+
+func Test_tryeFindSetting(t *testing.T) {
+	tests := []struct {
+		name     string
+		key      string
+		settings []debug.BuildSetting
+		want     string
+	}{{
+		name: "nil",
+		key:  vcsTime,
+		want: notFound,
+	}, {
+		name: "not found",
+		key:  vcsTime,
+		settings: []debug.BuildSetting{{
+			Key:   "foo",
+			Value: "bar",
+		}},
+		want: notFound,
+	}, {
+		name: "found",
+		key:  vcsTime,
+		settings: []debug.BuildSetting{{
+			Key:   vcsTime,
+			Value: "bar",
+		}},
+		want: "bar",
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tryeFindSetting(tt.key, tt.settings...); got != tt.want {
+				t.Errorf("tryeFindSetting() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
