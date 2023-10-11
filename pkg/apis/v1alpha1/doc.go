66fcc56f4d9f1e988b5076c0daa6b186ec09181a
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-11 09:06:52 +0200

    chore: add unit tests (#36)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/apis/v1alpha1/doc.go
+++ pkg/apis/v1alpha1/doc.go
@@ -1,4 +1,4 @@
-// Package v1alpha1 contains API Schema definitions for the v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the v1alpha1 API group.
 // +k8s:deepcopy-gen=package
 // +kubebuilder:object:generate=true
 // +groupName=chainsaw.kyverno.io
