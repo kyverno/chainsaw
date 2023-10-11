d3d97ca0a90f9d8cb532db1416c4609a3b157bec
Shubham Gupta <69793468+shubham-cmyk@users.noreply.github.com>
2023-10-11 13:12:59 +0530

    Add config elements (#37)
    * add more elements
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>
    
    * fix formatting
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>
    
    * push gen code
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>
    
    * changes
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>
    
    ---------
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>
    Co-authored-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/apis/v1alpha1/configuration_spec.go
+++ pkg/apis/v1alpha1/configuration_spec.go
@@ -4,10 +4,62 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type ReportFormatType string
+
+const (
+	JSONFormat ReportFormatType = "JSON"
+	XMLFormat  ReportFormatType = "XML"
+	NoReport   ReportFormatType = ""
+)
+
 // ConfigurationSpec contains the configuration used to run tests.
 type ConfigurationSpec struct {
 	// Timeout per test step.
 	// +optional
 	// +kubebuilder:default:="30s"
 	Timeout *metav1.Duration `json:"timeout,omitempty"`
+
+	// Directories containing test cases to run.
+	// +optional
+	TestDirs []string `json:"testDirs,omitempty"`
+
+	// If set, do not delete the resources after running the tests (implies SkipClusterDelete).
+	// +optional
+	SkipDelete bool `json:"skipDelete,omitempty"`
+
+	// StopOnFirstFailure determines whether the test should stop upon encountering the first failure.
+	// +optional
+	StopOnFirstFailure bool `json:"stopOnFirstFailure,omitempty"`
+
+	// The maximum number of tests to run at once.
+	// +kubebuilder:default:=8
+	// +kubebuilder:validation:Format:=int
+	Parallel int `json:"parallel,omitempty"`
+
+	// ReportFormat determines test report format (JSON|XML|nil) nil == no report.
+	// maps to report.Type, however we don't want generated.deepcopy to have reference to it.
+	// +optional
+	// +kubebuilder:validation:Enum=JSON;XML;
+	ReportFormat ReportFormatType `json:"reportFormat,omitempty"`
+
+	// ReportName defines the name of report to create. It defaults to "chainsaw-report".
+	// +optional
+	// +kubebuilder:default:="chainsaw-report"
+	ReportName string `json:"reportName,omitempty"`
+
+	// Namespace defines the namespace to use for tests.
+	// +optional
+	Namespace string `json:"namespace,omitempty"`
+
+	// Suppress is used to suppress logs.
+	// +optional
+	Suppress []string `json:"suppress,omitempty"`
+
+	// FullName makes use of the full test case folder path instead of the folder name.
+	// +optional
+	FullName bool `json:"fullName,omitempty"`
+
+	// SkipTestRegex is used to skip tests based on a regular expression.
+	// +optional
+	SkipTestRegex string `json:"skipTestRegex,omitempty"`
 }
