a1b5e28681051b29f88432d7c6dadc38029f68fe
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-10-14 22:31:41 +0530

    delete file
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/utils/kubernetes/kubernetes.go
+++
@@ -1,6 +0,0 @@
-package kubernetes
-
-// This determine if the resource is clustered or namespaced
-// if the resource is namespaced and doesn't have a namespace set, use the pet namespace
-// if the resource is namespaced and has a namespace set, use the namespace set
-// return namespace, name, error
