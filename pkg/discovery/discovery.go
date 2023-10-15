1e26ceebc5d2db3f691d9a9111098a38a413606e
Breteche <eddycharly@Breteches-MacBook-Air.local>
2023-10-15 21:13:30 +0200

    regex
    Signed-off-by: Breteche <eddycharly@Breteches-MacBook-Air.local>

--- pkg/discovery/discovery.go
+++ pkg/discovery/discovery.go
@@ -4,11 +4,15 @@ import (
 	"io/fs"
 	"os"
 	"path/filepath"
+	"regexp"
 
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
 	"github.com/kyverno/chainsaw/pkg/test"
+	"golang.org/x/exp/slices"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+var stepFileName = regexp.MustCompile(`^(\d\d)-(.*)\.(?:yaml|yml)$`)
 
 type Test struct {
 	*v1alpha1.Test
@@ -49,7 +53,7 @@ func DiscoverTests(fileName string, paths ...string) ([]Test, error) {
 	return tests, nil
 }
 
-func DiscoverTests2(fileName string, paths ...string) ([]Test, error) {
+func DiscoverTests2(paths ...string) ([]Test, error) {
 	var folders []string
 	for _, path := range paths {
 		if _, err := os.Lstat(path); err == nil {
@@ -74,14 +78,16 @@ func DiscoverTests2(fileName string, paths ...string) ([]Test, error) {
 		if err == nil {
 			var stepFiles []string
 			for _, file := range files {
-				// check file name pattern
+				fileName := file.Name()
 				if !file.IsDir() {
-					stepFiles = append(stepFiles, file.Name())
+					if stepFileName.MatchString(fileName) {
+						stepFiles = append(stepFiles, fileName)
+					}
 				}
 			}
 			// we found some steps
 			if len(stepFiles) != 0 {
-				// TODO sort steps
+				slices.Sort(stepFiles)
 				test := &v1alpha1.Test{
 					TypeMeta: metav1.TypeMeta{
 						APIVersion: "chainsaw.kyverno.io/v1alpha1",
@@ -91,16 +97,40 @@ func DiscoverTests2(fileName string, paths ...string) ([]Test, error) {
 						Name: filepath.Base(folder),
 					},
 				}
+				stepsMap := map[string]v1alpha1.TestStepSpec{}
 				for _, stepFile := range stepFiles {
 					// TODO: consider case the file contains a test step
-					// TODO: if it's not a test step decode operation from file name
-					test.Spec.Steps = append(test.Spec.Steps, v1alpha1.TestStepSpec{
-						Apply: []v1alpha1.Apply{{
-							FileRef: v1alpha1.FileRef{
-								File: stepFile,
-							},
-						}},
-					})
+					fileRef := v1alpha1.FileRef{
+						File: stepFile,
+					}
+					groups := stepFileName.FindStringSubmatch(stepFile)
+					step, ok := stepsMap[groups[1]]
+					if !ok {
+						step = v1alpha1.TestStepSpec{}
+					}
+					switch groups[2] {
+					case "assert":
+						step.Assert = append(step.Assert, v1alpha1.Assert{
+							FileRef: fileRef,
+						})
+					case "error":
+						step.Error = append(step.Error, v1alpha1.Error{
+							FileRef: fileRef,
+						})
+					default:
+						step.Apply = append(step.Apply, v1alpha1.Apply{
+							FileRef: fileRef,
+						})
+					}
+					stepsMap[groups[1]] = step
+				}
+				keys := make([]string, 0, len(stepsMap))
+				for k := range stepsMap {
+					keys = append(keys, k)
+				}
+				slices.Sort(keys)
+				for _, key := range keys {
+					test.Spec.Steps = append(test.Spec.Steps, stepsMap[key])
 				}
 				tests = append(tests, Test{
 					Test:     test,
