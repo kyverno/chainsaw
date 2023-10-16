fd9c1c2583cdc7f3265eb77f41e591f357654f0c
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-10-16 17:22:08 +0530

    try lint
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/client/assert.go
+++ pkg/client/assert.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+
 	dClient "k8s.io/client-go/discovery"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
