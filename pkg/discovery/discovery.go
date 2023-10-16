f82144fd89d984c95275bdd14d946b32d65ada22
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-16 17:02:20 +0200

    fix: improve discovery (#66)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/discovery/discovery.go
+++ pkg/discovery/discovery.go
@@ -12,6 +12,7 @@ import (
 	"github.com/kyverno/chainsaw/pkg/test"
 	"golang.org/x/exp/slices"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 var stepFileName = regexp.MustCompile(`^(\d\d)-(.*)\.(?:yaml|yml)$`)
@@ -22,41 +23,104 @@ type Test struct {
 }
 
 func DiscoverTests(fileName string, paths ...string) ([]Test, error) {
-	var files []string
-	for _, path := range paths {
-		if _, err := os.Lstat(path); err == nil {
-			err := filepath.Walk(path, func(file string, info fs.FileInfo, err error) error {
-				if err != nil {
-					return err
-				}
-				if info.Name() == fileName {
-					files = append(files, file)
-				}
-				return nil
-			})
-			if err != nil {
-				return nil, err
-			}
-		}
+	folders, err := discoverFolders(paths...)
+	if err != nil {
+		return nil, err
 	}
 	var tests []Test
-	for _, file := range files {
-		apiTests, err := test.Load(file)
-		if err == nil {
-			basePath := filepath.Dir(file)
-			for _, test := range apiTests {
-				tests = append(tests, Test{
-					Test:     test,
-					BasePath: basePath,
-				})
+	for _, folder := range folders {
+		file := filepath.Join(folder, fileName)
+		// check if we have a full test file present
+		if _, err := os.Lstat(file); err == nil {
+			apiTests, err := test.Load(file)
+			if err == nil {
+				basePath := filepath.Dir(file)
+				for _, test := range apiTests {
+					tests = append(tests, Test{
+						Test:     test,
+						BasePath: basePath,
+					})
+				}
+			}
+		} else {
+			// list files in folder
+			files, err := os.ReadDir(folder)
+			if err == nil {
+				var stepFiles []string
+				for _, file := range files {
+					fileName := file.Name()
+					if !file.IsDir() {
+						if stepFileName.MatchString(fileName) {
+							stepFiles = append(stepFiles, fileName)
+						}
+					}
+				}
+				// we found some steps
+				if len(stepFiles) != 0 {
+					slices.Sort(stepFiles)
+					test := &v1alpha1.Test{
+						TypeMeta: metav1.TypeMeta{
+							APIVersion: "chainsaw.kyverno.io/v1alpha1",
+							Kind:       "Test",
+						},
+						ObjectMeta: metav1.ObjectMeta{
+							Name: filepath.Base(folder),
+						},
+					}
+					stepsMap := map[string]v1alpha1.TestStepSpec{}
+					for _, stepFile := range stepFiles {
+						groups := stepFileName.FindStringSubmatch(stepFile)
+						if steps, err := step.Load(filepath.Join(folder, stepFile)); err != nil {
+							fileRef := v1alpha1.FileRef{
+								File: stepFile,
+							}
+							step, ok := stepsMap[groups[1]]
+							if !ok {
+								step = v1alpha1.TestStepSpec{}
+							}
+							switch groups[2] {
+							case "assert":
+								step.Assert = append(step.Assert, v1alpha1.Assert{
+									FileRef: fileRef,
+								})
+							case "error":
+								step.Error = append(step.Error, v1alpha1.Error{
+									FileRef: fileRef,
+								})
+							default:
+								step.Apply = append(step.Apply, v1alpha1.Apply{
+									FileRef: fileRef,
+								})
+							}
+							stepsMap[groups[1]] = step
+						} else {
+							if len(steps) != 1 {
+								return nil, fmt.Errorf("more than one test step found in %s", filepath.Join(folder, stepFile))
+							}
+							stepsMap[groups[1]] = steps[0].Spec
+						}
+					}
+					keys := make([]string, 0, len(stepsMap))
+					for k := range stepsMap {
+						keys = append(keys, k)
+					}
+					slices.Sort(keys)
+					for _, key := range keys {
+						test.Spec.Steps = append(test.Spec.Steps, stepsMap[key])
+					}
+					tests = append(tests, Test{
+						Test:     test,
+						BasePath: folder,
+					})
+				}
 			}
 		}
 	}
 	return tests, nil
 }
 
-func DiscoverTests2(paths ...string) ([]Test, error) {
-	var folders []string
+func discoverFolders(paths ...string) ([]string, error) {
+	folders := sets.New[string]()
 	for _, path := range paths {
 		if _, err := os.Lstat(path); err == nil {
 			err := filepath.Walk(path, func(file string, info fs.FileInfo, err error) error {
@@ -64,7 +128,7 @@ func DiscoverTests2(paths ...string) ([]Test, error) {
 					return err
 				}
 				if info.IsDir() {
-					folders = append(folders, file)
+					folders.Insert(file)
 				}
 				return nil
 			})
@@ -73,80 +137,5 @@ func DiscoverTests2(paths ...string) ([]Test, error) {
 			}
 		}
 	}
-	var tests []Test
-	for _, folder := range folders {
-		// list files in folder
-		files, err := os.ReadDir(folder)
-		if err == nil {
-			var stepFiles []string
-			for _, file := range files {
-				fileName := file.Name()
-				if !file.IsDir() {
-					if stepFileName.MatchString(fileName) {
-						stepFiles = append(stepFiles, fileName)
-					}
-				}
-			}
-			// we found some steps
-			if len(stepFiles) != 0 {
-				slices.Sort(stepFiles)
-				test := &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: filepath.Base(folder),
-					},
-				}
-				stepsMap := map[string]v1alpha1.TestStepSpec{}
-				for _, stepFile := range stepFiles {
-					groups := stepFileName.FindStringSubmatch(stepFile)
-					// TODO: consider case the file contains a test step
-					if steps, err := step.Load(filepath.Join(folder, stepFile)); err != nil {
-						fileRef := v1alpha1.FileRef{
-							File: stepFile,
-						}
-						step, ok := stepsMap[groups[1]]
-						if !ok {
-							step = v1alpha1.TestStepSpec{}
-						}
-						switch groups[2] {
-						case "assert":
-							step.Assert = append(step.Assert, v1alpha1.Assert{
-								FileRef: fileRef,
-							})
-						case "error":
-							step.Error = append(step.Error, v1alpha1.Error{
-								FileRef: fileRef,
-							})
-						default:
-							step.Apply = append(step.Apply, v1alpha1.Apply{
-								FileRef: fileRef,
-							})
-						}
-						stepsMap[groups[1]] = step
-					} else {
-						if len(steps) != 1 {
-							return nil, fmt.Errorf("more than one test step found in %s", filepath.Join(folder, stepFile))
-						}
-						stepsMap[groups[1]] = steps[0].Spec
-					}
-				}
-				keys := make([]string, 0, len(stepsMap))
-				for k := range stepsMap {
-					keys = append(keys, k)
-				}
-				slices.Sort(keys)
-				for _, key := range keys {
-					test.Spec.Steps = append(test.Spec.Steps, stepsMap[key])
-				}
-				tests = append(tests, Test{
-					Test:     test,
-					BasePath: folder,
-				})
-			}
-		}
-	}
-	return tests, nil
+	return sets.List(folders), nil
 }
