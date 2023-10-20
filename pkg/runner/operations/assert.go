3c9cd719eaaf90e8cc3790f69bb417aebe69e0cc
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-10-20 19:49:32 +0530

    meaninful error here
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/assert.go
+++ pkg/runner/operations/assert.go
@@ -2,6 +2,7 @@ package operations
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/kyverno/chainsaw/pkg/client"
 	"github.com/kyverno/chainsaw/pkg/match"
@@ -12,19 +13,45 @@ import (
 
 func Assert(ctx context.Context, expected unstructured.Unstructured, c client.Client) error {
 	return wait.PollUntilContextCancel(ctx, interval, false, func(ctx context.Context) (bool, error) {
-		if candidates, err := read(ctx, expected, c); err != nil {
+		candidates, err := read(ctx, expected, c)
+		if err != nil {
 			if errors.IsNotFound(err) {
 				return false, nil
 			}
 			return false, err
-		} else {
-			for _, candidate := range candidates {
-				// at least one must match
-				if err := match.Match(expected.UnstructuredContent(), candidate.UnstructuredContent()); err == nil {
-					return true, nil
-				}
+		}
+		if len(candidates) == 0 {
+			return false, fmt.Errorf("no resource found")
+		}
+
+		var mismatchErrors []error
+		matchedCount := 0
+		for _, candidate := range candidates {
+			err := match.Match(expected.UnstructuredContent(), candidate.UnstructuredContent())
+			if err != nil {
+				mismatchErrors = append(mismatchErrors, fmt.Errorf("resource %s/%s mismatch: %s", candidate.GetNamespace(), candidate.GetName(), err.Error()))
+			} else {
+				matchedCount++
 			}
 		}
-		return false, nil
+
+		// If some matched and some not
+		if matchedCount > 0 && matchedCount < len(candidates) {
+			errMsg := fmt.Sprintf("%d of %d resources matched. Mismatches:", matchedCount, len(candidates))
+			for _, err := range mismatchErrors {
+				errMsg += "\n" + err.Error()
+			}
+			return false, fmt.Errorf(errMsg)
+		}
+
+		// If none of the resources matched.
+		if matchedCount == 0 {
+			errMsg := "All resources mismatched: "
+			for _, err := range mismatchErrors {
+				errMsg += "\n" + err.Error()
+			}
+			return false, fmt.Errorf(errMsg)
+		}
+		return true, nil
 	})
 }
