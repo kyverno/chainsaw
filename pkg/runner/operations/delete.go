fa478164ed70ef291659d15e49d090ec31782e8d
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-25 00:52:10 +0200

    fix: remove wrong logic (#181)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/operations/delete.go
+++ pkg/runner/operations/delete.go
@@ -18,30 +18,26 @@ func Delete(ctx context.Context, expected unstructured.Unstructured, c client.Cl
 		return err
 	}
 	for i := range candidates {
-		if candidates[i].GetName() != "kube-root-ca.crt" {
-			err := c.Delete(ctx, &candidates[i])
-			if err != nil && !errors.IsNotFound(err) {
-				return err
-			}
+		err := c.Delete(ctx, &candidates[i])
+		if err != nil && !errors.IsNotFound(err) {
+			return err
 		}
 	}
 	gvk := expected.GetObjectKind().GroupVersionKind()
 	for i := range candidates {
-		if candidates[i].GetName() != "kube-root-ca.crt" {
-			if err := wait.PollUntilContextCancel(ctx, interval, true, func(ctx context.Context) (bool, error) {
-				var actual unstructured.Unstructured
-				actual.SetGroupVersionKind(gvk)
-				err := c.Get(ctx, client.ObjectKey(&candidates[i]), &actual)
-				if err != nil {
-					if errors.IsNotFound(err) {
-						return true, nil
-					}
-					return false, err
+		if err := wait.PollUntilContextCancel(ctx, interval, true, func(ctx context.Context) (bool, error) {
+			var actual unstructured.Unstructured
+			actual.SetGroupVersionKind(gvk)
+			err := c.Get(ctx, client.ObjectKey(&candidates[i]), &actual)
+			if err != nil {
+				if errors.IsNotFound(err) {
+					return true, nil
 				}
-				return false, nil
-			}); err != nil {
-				return err
+				return false, err
 			}
+			return false, nil
+		}); err != nil {
+			return err
 		}
 	}
 	return nil
