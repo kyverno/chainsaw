b5be71534d902a5ba5d2b364ed1912111e03b69f
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-25 16:24:55 +0200

    refactor: runner client
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/client/client.go
+++ pkg/runner/client/client.go
@@ -3,64 +3,68 @@ package client
 import (
 	"context"
 
-	"github.com/kyverno/chainsaw/pkg/client"
-	"github.com/kyverno/chainsaw/pkg/runner/logging"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func New(logger logging.Logger, inner client.Client) client.Client {
-	return &runnerClient{
-		logger: logger,
-		inner:  inner,
-	}
+type Client interface {
+	Delete(context.Context, ctrlclient.Object) error
+	Apply(context.Context, ctrlclient.Object) error
+	Assert(context.Context, unstructured.Unstructured) error
+	Error(context.Context, unstructured.Unstructured) error
 }
 
-type runnerClient struct {
-	logger logging.Logger
-	inner  client.Client
-}
+// func New(logger logging.Logger, inner client.Client) client.Client {
+// 	return &runnerClient{
+// 		logger: logger,
+// 		inner:  inner,
+// 	}
+// }
 
-func (c *runnerClient) Create(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.CreateOption) error {
-	c.log("create", client.ObjectKey(obj), obj)
-	err := c.inner.Create(ctx, obj, opts...)
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// type runnerClient struct {
+// 	logger logging.Logger
+// 	inner  client.Client
+// }
 
-func (c *runnerClient) Delete(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.DeleteOption) error {
-	return c.inner.Delete(ctx, obj, opts...)
-}
+// func (c *runnerClient) Create(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.CreateOption) error {
+// 	c.log("create", client.ObjectKey(obj), obj)
+// 	err := c.inner.Create(ctx, obj, opts...)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
 
-func (c *runnerClient) DeleteAllOf(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.DeleteAllOfOption) error {
-	return c.inner.DeleteAllOf(ctx, obj, opts...)
-}
+// func (c *runnerClient) Delete(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.DeleteOption) error {
+// 	return c.inner.Delete(ctx, obj, opts...)
+// }
 
-func (c *runnerClient) Get(ctx context.Context, key types.NamespacedName, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
-	return c.inner.Get(ctx, key, obj, opts...)
-}
+// func (c *runnerClient) DeleteAllOf(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.DeleteAllOfOption) error {
+// 	return c.inner.DeleteAllOf(ctx, obj, opts...)
+// }
 
-func (c *runnerClient) IsObjectNamespaced(obj runtime.Object) (bool, error) {
-	return c.inner.IsObjectNamespaced(obj)
-}
+// func (c *runnerClient) Get(ctx context.Context, key types.NamespacedName, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
+// 	return c.inner.Get(ctx, key, obj, opts...)
+// }
 
-func (c *runnerClient) List(ctx context.Context, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error {
-	return c.inner.List(ctx, list, opts...)
-}
+// func (c *runnerClient) IsObjectNamespaced(obj runtime.Object) (bool, error) {
+// 	return c.inner.IsObjectNamespaced(obj)
+// }
 
-func (c *runnerClient) Patch(ctx context.Context, obj ctrlclient.Object, patch ctrlclient.Patch, opts ...ctrlclient.PatchOption) error {
-	c.log("patch", client.ObjectKey(obj), obj)
-	return c.inner.Patch(ctx, obj, patch, opts...)
-}
+// func (c *runnerClient) List(ctx context.Context, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error {
+// 	return c.inner.List(ctx, list, opts...)
+// }
 
-func (c *runnerClient) Update(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.UpdateOption) error {
-	c.log("update", client.ObjectKey(obj), obj)
-	return c.inner.Update(ctx, obj, opts...)
-}
+// func (c *runnerClient) Patch(ctx context.Context, obj ctrlclient.Object, patch ctrlclient.Patch, opts ...ctrlclient.PatchOption) error {
+// 	c.log("patch", client.ObjectKey(obj), obj)
+// 	return c.inner.Patch(ctx, obj, patch, opts...)
+// }
 
-func (c *runnerClient) log(op string, key ctrlclient.ObjectKey, obj ctrlclient.Object) {
-	// logging.ResourceOp(c.logger, op, key, obj)
-}
+// func (c *runnerClient) Update(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.UpdateOption) error {
+// 	c.log("update", client.ObjectKey(obj), obj)
+// 	return c.inner.Update(ctx, obj, opts...)
+// }
+
+// func (c *runnerClient) log(op string, key ctrlclient.ObjectKey, obj ctrlclient.Object) {
+// 	// logging.ResourceOp(c.logger, op, key, obj)
+// }
--- pkg/runner/step.go
+++ pkg/runner/step.go
@@ -5,17 +5,23 @@ import (
 	"testing"
 
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
-	"github.com/kyverno/chainsaw/pkg/client"
 	"github.com/kyverno/chainsaw/pkg/resource"
+	runnerclient "github.com/kyverno/chainsaw/pkg/runner/client"
 	"github.com/kyverno/chainsaw/pkg/runner/logging"
-	"github.com/kyverno/chainsaw/pkg/runner/operations"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-func executeStep(t *testing.T, logger logging.Logger, ctx Context, basePath string, config v1alpha1.ConfigurationSpec, test v1alpha1.TestSpec, step v1alpha1.TestSpecStep) {
+func executeStep(
+	t *testing.T,
+	logger logging.Logger,
+	client runnerclient.Client,
+	ctx Context,
+	basePath string,
+	config v1alpha1.ConfigurationSpec,
+	test v1alpha1.TestSpec,
+	step v1alpha1.TestSpecStep,
+) {
 	t.Helper()
-	c := ctx.clientFactory(logger)
 	stepCtx, cancel := timeoutCtx(config, test, step.Spec)
 	defer cancel()
 	for _, delete := range step.Spec.Delete {
@@ -29,24 +35,24 @@ func executeStep(t *testing.T, logger logging.Logger, ctx Context, basePath stri
 			logger.Log(err)
 			t.FailNow()
 		}
-		if err := operations.Delete(stepCtx, logger, &resource, c); err != nil {
+		if err := client.Delete(stepCtx, &resource); err != nil {
 			logger.Log(err)
 			t.FailNow()
 		}
 	}
-	var cleanup operations.CleanupFunc
-	if skip := skipDelete(config, test, step.Spec); skip == nil || !*skip {
-		cleanup = func(obj ctrlclient.Object, c client.Client) {
-			t.Cleanup(func() {
-				cleanupCtx, cancel := timeoutCtx(config, test, step.Spec)
-				defer cancel()
-				if err := operations.Delete(cleanupCtx, logger, obj, c); err != nil {
-					logger.Log(err)
-					t.FailNow()
-				}
-			})
-		}
-	}
+	// var cleanup operations.CleanupFunc
+	// if skip := skipDelete(config, test, step.Spec); skip == nil || !*skip {
+	// 	cleanup = func(obj ctrlclient.Object, c client.Client) {
+	// 		t.Cleanup(func() {
+	// 			cleanupCtx, cancel := timeoutCtx(config, test, step.Spec)
+	// 			defer cancel()
+	// 			if err := client.Delete(cleanupCtx, logger, obj, c); err != nil {
+	// 				logger.Log(err)
+	// 				t.FailNow()
+	// 			}
+	// 		})
+	// 	}
+	// }
 	for _, apply := range step.Spec.Apply {
 		resources, err := resource.Load(filepath.Join(basePath, apply.File))
 		if err != nil {
@@ -59,7 +65,7 @@ func executeStep(t *testing.T, logger logging.Logger, ctx Context, basePath stri
 				logger.Log(err)
 				t.FailNow()
 			}
-			if err := operations.Apply(stepCtx, logger, resource, c, cleanup); err != nil {
+			if err := client.Apply(stepCtx, resource); err != nil {
 				logger.Log(err)
 				t.FailNow()
 			}
@@ -77,7 +83,7 @@ func executeStep(t *testing.T, logger logging.Logger, ctx Context, basePath stri
 				logger.Log(err)
 				t.FailNow()
 			}
-			if err := operations.Assert(stepCtx, logger, resources[i], c); err != nil {
+			if err := client.Assert(stepCtx, resources[i]); err != nil {
 				logger.Log(err)
 				t.FailNow()
 			}
@@ -95,7 +101,7 @@ func executeStep(t *testing.T, logger logging.Logger, ctx Context, basePath stri
 				logger.Log(err)
 				t.FailNow()
 			}
-			if err := operations.Error(stepCtx, logger, resources[i], c); err != nil {
+			if err := client.Error(stepCtx, resources[i]); err != nil {
 				logger.Log(err)
 				t.FailNow()
 			}
