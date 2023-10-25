77ec482304aa93032f1301cc7579ec659aead922
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-25 10:09:29 +0200

    fix: error implementation (#186)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/operations/error.go
+++ pkg/runner/operations/error.go
@@ -2,25 +2,52 @@ package operations
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/kyverno/chainsaw/pkg/client"
-	"k8s.io/apimachinery/pkg/api/errors"
+	"github.com/kyverno/chainsaw/pkg/match"
+	"go.uber.org/multierr"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 func Error(ctx context.Context, expected unstructured.Unstructured, c client.Client) error {
-	return wait.PollUntilContextCancel(ctx, interval, false, func(ctx context.Context) (bool, error) {
-		candidates, err := read(ctx, expected, c)
-		if err != nil {
-			if errors.IsNotFound(err) {
+	var lastErrs []error
+	err := wait.PollUntilContextCancel(ctx, interval, false, func(ctx context.Context) (_ bool, err error) {
+		var errs []error
+		defer func() {
+			// record last errors only if there was no real error
+			if err == nil {
+				lastErrs = errs
+			}
+		}()
+		if candidates, err := read(ctx, expected, c); err != nil {
+			if kerrors.IsNotFound(err) {
 				return true, nil
 			}
 			return false, err
-		}
-		if len(candidates) == 0 {
+		} else if len(candidates) == 0 {
 			return true, nil
+		} else {
+			for i := range candidates {
+				candidate := candidates[i]
+				if err := match.Match(expected.UnstructuredContent(), candidate.UnstructuredContent()); err == nil {
+					// at least one match found
+					errs = append(errs, fmt.Errorf("found an actual resource matching expectation (%s/%s %s)", candidate.GetAPIVersion(), candidate.GetKind(), client.ObjectKey(&candidate)))
+				}
+			}
+			return len(errs) == 0, nil
 		}
-		return false, nil
 	})
+	// if no error, return success
+	if err == nil {
+		return nil
+	}
+	// eventually return a combination of last errors
+	if len(lastErrs) != 0 {
+		return multierr.Combine(lastErrs...)
+	}
+	// return received error
+	return err
 }
