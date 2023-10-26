e9ab30235747fd26cdba819e268d9d3f327cbcf1
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-10-26 20:31:59 +0530

    some fixes
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/command.go
+++ pkg/runner/operations/command.go
@@ -4,38 +4,47 @@ import (
 	"context"
 	"fmt"
 	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
 )
 
 func ExecuteCommand(cmdSpec v1alpha1.Command, namespace string) error {
+	isScript := cmdSpec.Script != ""
 	var cmdStr string
-	if cmdSpec.Script != "" {
+	if isScript {
 		cmdStr = cmdSpec.Script
 	} else {
 		cmdStr = cmdSpec.Command
 	}
 
-	// If namespaced, add `--namespace` flag.
+	// Append namespace flag if necessary
 	if cmdSpec.Namespaced {
 		cmdStr = fmt.Sprintf("%s --namespace %s", cmdStr, namespace)
 	}
 
-	// Handle timeout if provided
-	var ctx context.Context = context.Background()
-	var cancel context.CancelFunc
-
+	// Setup context for timeout
+	ctx := context.Background()
 	if cmdSpec.Timeout > 0 {
+		var cancel context.CancelFunc
 		ctx, cancel = context.WithTimeout(ctx, time.Duration(cmdSpec.Timeout)*time.Second)
 		defer cancel()
 	}
 
-	command := exec.CommandContext(ctx, "sh", "-c", cmdStr)
+	// Create the appropriate command based on the input
+	var command *exec.Cmd
+	if isScript {
+		command = exec.CommandContext(ctx, "sh", "-c", cmdStr)
+	} else {
+		args := strings.Fields(cmdStr)
+		command = exec.CommandContext(ctx, args[0], args[1:]...)
+	}
 
-	// Handle output (if not skipping)
+	// Execute and handle output
 	var out []byte
 	var err error
+
 	if !cmdSpec.SkipLogOutput {
 		out, err = command.CombinedOutput()
 		if err != nil {
@@ -44,7 +53,7 @@ func ExecuteCommand(cmdSpec v1alpha1.Command, namespace string) error {
 			}
 			return fmt.Errorf("command failed: %s\n%s", err, out)
 		}
-		fmt.Printf("Command output: %s\n", out) // i have to consider this as a log output or not
+		fmt.Printf("Command output: %s\n", out)
 	} else {
 		err = command.Run()
 		if err != nil {
