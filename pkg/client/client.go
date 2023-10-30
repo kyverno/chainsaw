dce5eca34acb32dcdf78a19b4bc738b45adb342b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-10-30 21:41:51 +0100

    refactor: reduce client interface scope
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,14 +1,36 @@
 package client
 
 import (
+	"context"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/rest"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type Client interface {
-	ctrlclient.Reader
-	ctrlclient.Writer
+	// Get retrieves an obj for the given object key from the Kubernetes Cluster.
+	// obj must be a struct pointer so that obj can be updated with the response
+	// returned by the Server.
+	Get(ctx context.Context, key ctrlclient.ObjectKey, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error
+
+	// List retrieves list of objects for a given namespace and list options. On a
+	// successful call, Items field in the list will be populated with the
+	// result returned from the server.
+	List(ctx context.Context, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error
+
+	// Create saves the object obj in the Kubernetes cluster. obj must be a
+	// struct pointer so that obj can be updated with the content returned by the Server.
+	Create(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.CreateOption) error
+
+	// Delete deletes the given obj from Kubernetes cluster.
+	Delete(ctx context.Context, obj ctrlclient.Object, opts ...ctrlclient.DeleteOption) error
+
+	// Patch patches the given obj in the Kubernetes cluster. obj must be a
+	// struct pointer so that obj can be updated with the content returned by the Server.
+	Patch(ctx context.Context, obj ctrlclient.Object, patch ctrlclient.Patch, opts ...ctrlclient.PatchOption) error
+
+	// IsObjectNamespaced returns true if the GroupVersionKind of the object is namespaced.
 	IsObjectNamespaced(obj runtime.Object) (bool, error)
 }
 
