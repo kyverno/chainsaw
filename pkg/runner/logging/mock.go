68fb63550f5141537a21e55424b27986b324eb0b
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-01 19:57:53 +0530

    assert_test.go
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/logging/mock.go
+++ pkg/runner/logging/mock.go
@@ -3,6 +3,7 @@ package logging
 import (
 	"fmt"
 
+	"github.com/fatih/color"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -52,3 +53,16 @@ func (tl *TestLogger) Log(args ...interface{}) {
 }
 
 func (tl *TestLogger) Helper() {}
+
+type MockLogger struct {
+	Logs []string
+}
+
+func (m *MockLogger) WithResource(resource ctrlclient.Object) Logger {
+	return m
+}
+
+func (m *MockLogger) Log(operation string, color *color.Color, args ...interface{}) {
+	message := fmt.Sprintf("%s: %v", operation, args)
+	m.Logs = append(m.Logs, message)
+}
