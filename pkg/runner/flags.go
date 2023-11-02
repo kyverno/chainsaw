aa6c8d85f7f072bfd7e9585047364534de89f58a
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-11-02 12:01:23 +0100

    chore: add runner flags unit tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/flags.go
+++ pkg/runner/flags.go
@@ -2,42 +2,18 @@ package runner
 
 import (
 	"flag"
-	"fmt"
-	"strconv"
 	"testing"
 
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
+	"github.com/kyverno/chainsaw/pkg/runner/flags"
 )
 
 func setupFlags(config v1alpha1.ConfigurationSpec) error {
 	testing.Init()
-	if err := flag.Set("test.v", "true"); err != nil {
-		return err
-	}
-	if config.Parallel != nil {
-		if err := flag.Set("test.parallel", strconv.Itoa(*config.Parallel)); err != nil {
+	for k, v := range flags.GetFlags(config) {
+		if err := flag.Set(k, v); err != nil {
 			return err
 		}
-	}
-	if err := flag.Set("test.failfast", fmt.Sprint(config.FailFast)); err != nil {
-		return err
-	}
-	if err := flag.Set("test.paniconexit0", "true"); err != nil {
-		return err
-	}
-	if err := flag.Set("test.fullpath", "false"); err != nil {
-		return err
-	}
-	if config.RepeatCount != nil {
-		if err := flag.Set("test.count", strconv.Itoa(*config.RepeatCount)); err != nil {
-			return err
-		}
-	}
-	if err := flag.Set("test.run", config.IncludeTestRegex); err != nil {
-		return err
-	}
-	if err := flag.Set("test.skip", config.ExcludeTestRegex); err != nil {
-		return err
 	}
 	flag.Parse()
 	return nil
