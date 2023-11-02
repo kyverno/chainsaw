830b5182f349fff5beb4a9e06f2dd8d5e19c1fa4
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-02 18:40:37 +0530

    fix test/load
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/test/load_test.go
+++ pkg/test/load_test.go
@@ -54,22 +54,24 @@ func TestLoad(t *testing.T) {
 			Spec: v1alpha1.TestSpec{
 				Steps: []v1alpha1.TestSpecStep{{
 					Spec: v1alpha1.TestStepSpec{
-						Operations: v1alpha1.Operations{
+						Operations: []v1alpha1.Operations{{
 							Apply: []v1alpha1.Apply{{
 								FileRef: v1alpha1.FileRef{
 									File: "foo.yaml",
 								},
 							}},
 						},
+						},
 					},
 				}, {
 					Spec: v1alpha1.TestStepSpec{
-						Operations: v1alpha1.Operations{
+						Operations: []v1alpha1.Operations{{
 							Assert: []v1alpha1.Assert{{
 								FileRef: v1alpha1.FileRef{
 									File: "bar.yaml",
 								},
 							}},
+						},
 						},
 					},
 				}},
