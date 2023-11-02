18b2dc18b99578d102876bce0115998a78f76a95
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-11-02 01:35:07 +0100

    chore: add kuttl migrate command tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/kuttl/migrate/command_test.go
+++ pkg/commands/kuttl/migrate/command_test.go
@@ -3,58 +3,73 @@ package migrate
 import (
 	"bytes"
 	"io"
-	"strings"
+	"os"
 	"testing"
 
+	"github.com/kyverno/chainsaw/pkg/commands/root"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCommand(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	err := cmd.Execute()
-	assert.Error(t, err)
-}
-
-func TestCommandWithArgs(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	cmd.SetArgs([]string{"foo"})
-	err := cmd.Execute()
-	assert.NoError(t, err)
-}
-
-func TestCommandWithInvalidArg(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	cmd.SetArgs([]string{"foo"})
-	err := cmd.Execute()
-	assert.NoError(t, err)
-}
-
-func TestCommandWithInvalidFlag(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	b := bytes.NewBufferString("")
-	cmd.SetErr(b)
-	cmd.SetArgs([]string{"--xxx"})
-	err := cmd.Execute()
-	assert.Error(t, err)
-	out, err := io.ReadAll(b)
-	assert.NoError(t, err)
-	expected := `Error: unknown flag: --xxx`
-	assert.Equal(t, strings.TrimSpace(expected), strings.TrimSpace(string(out)))
-}
-
-func TestCommandHelp(t *testing.T) {
-	cmd := Command()
-	assert.NotNil(t, cmd)
-	b := bytes.NewBufferString("")
-	cmd.SetOut(b)
-	cmd.SetArgs([]string{"--help"})
-	err := cmd.Execute()
-	assert.NoError(t, err)
-	out, err := io.ReadAll(b)
-	assert.NoError(t, err)
-	assert.True(t, strings.HasPrefix(string(out), cmd.Long))
+func Test_Execute(t *testing.T) {
+	tests := []struct {
+		name    string
+		args    []string
+		wantErr bool
+		out     string
+	}{{
+		name: "help",
+		args: []string{
+			"migrate",
+			"--help",
+		},
+		out:     "../../../../testdata/commands/kuttl/migrate/help.txt",
+		wantErr: false,
+	}, {
+		name: "migrate",
+		args: []string{
+			"migrate",
+			"../../../../testdata/kuttl",
+		},
+		out:     "../../../../testdata/commands/kuttl/migrate/out.txt",
+		wantErr: false,
+	}, {
+		name: "migrate save",
+		args: []string{
+			"migrate",
+			"../../../../testdata/kuttl",
+			"--save",
+		},
+		out:     "../../../../testdata/commands/kuttl/migrate/out-save.txt",
+		wantErr: false,
+	}, {
+		name: "unknow flag",
+		args: []string{
+			"migrate",
+			"--foo",
+		},
+		wantErr: true,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cmd := root.Command()
+			cmd.AddCommand(Command())
+			assert.NotNil(t, cmd)
+			cmd.SetArgs(tt.args)
+			out := bytes.NewBufferString("")
+			cmd.SetOut(out)
+			err := cmd.Execute()
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			actual, err := io.ReadAll(out)
+			assert.NoError(t, err)
+			if tt.out != "" {
+				expected, err := os.ReadFile(tt.out)
+				assert.NoError(t, err)
+				assert.Equal(t, string(expected), string(actual))
+			}
+		})
+	}
 }
