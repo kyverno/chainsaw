dfbdefa70224c1242823db36157fa17bb429a42d
Shubham Gupta <69793468+shubham-cmyk@users.noreply.github.com>
2023-11-04 02:20:03 +0530

    test: 100% Coverage pkg/runner/names (#367)
    * 100% Coverage
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>
    
    * fix lint
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>
    
    ---------
    
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/names/test_test.go
+++ pkg/runner/names/test_test.go
@@ -1,6 +1,7 @@
 package names
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"testing"
@@ -99,6 +100,56 @@ func TestTest(t *testing.T) {
 			} else {
 				assert.NoError(t, err)
 				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestHelpTest(t *testing.T) {
+	testCases := []struct {
+		name           string
+		workingDirFunc workignDirInterface
+		absPathFunc    absolutePathInterface
+		relPathFunc    relativePathInterface
+		expectedErrMsg string
+	}{
+		{
+			name:           "ErrorGettingWorkingDirectory",
+			workingDirFunc: func() (string, error) { return "", errors.New("working directory error") },
+			expectedErrMsg: "failed to get current working dir (working directory error)",
+		},
+		{
+			name:           "ErrorGettingAbsolutePath",
+			absPathFunc:    func(string) (string, error) { return "", errors.New("absolute path error") },
+			expectedErrMsg: "failed to compute absolute path for /some/path (absolute path error)",
+		},
+		{
+			name:           "ErrorGettingRelativePath",
+			workingDirFunc: func() (string, error) { return "/correct/path", nil },
+			absPathFunc:    func(string) (string, error) { return "/correct/path/subdir", nil },
+			relPathFunc:    func(string, string) (string, error) { return "", errors.New("relative path error") },
+			expectedErrMsg: "failed to compute relative path from /correct/path to /correct/path/subdir (relative path error)",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			test := discovery.Test{
+				BasePath: "/some/path",
+				Test: &v1alpha1.Test{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "foo",
+					},
+				},
+			}
+
+			_, err := helpTest(test, tc.workingDirFunc, tc.absPathFunc, tc.relPathFunc)
+
+			if err == nil {
+				t.Fatalf("%s: expected an error but got none", tc.name)
+			}
+			if err.Error() != tc.expectedErrMsg {
+				t.Errorf("%s: expected error message '%v', got '%v'", tc.name, tc.expectedErrMsg, err.Error())
 			}
 		})
 	}
