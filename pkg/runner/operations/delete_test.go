c27b45467273a92a9ad32568cc0d0ee729dd9f1c
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-07 03:27:47 +0530

    delete_test.go
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/delete_test.go
+++ pkg/runner/operations/delete_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	tclient "github.com/kyverno/chainsaw/pkg/client/testing"
 	"github.com/kyverno/chainsaw/pkg/runner/logging"
@@ -30,66 +31,87 @@ func Test_operationDelete(t *testing.T) {
 		client       *tclient.FakeClient
 		expectedErr  error
 		expectedLogs []string
-	}{{
-		name:   "not found",
-		object: pod,
-		client: &tclient.FakeClient{
-			GetFn: func(_ context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, _ ...ctrlclient.GetOption) error {
-				return errors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), key.Name)
-			},
-			DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
-				return nil
-			},
-		},
-		expectedErr:  nil,
-		expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [DONE]"},
-	}, {
-		name:   "failed get",
-		object: pod.DeepCopy(),
-		client: &tclient.FakeClient{
-			GetFn: func(_ context.Context, _ int, _ ctrlclient.ObjectKey, _ ctrlclient.Object, _ ...ctrlclient.GetOption) error {
-				return errors.NewInternalError(fmt.Errorf("failed to get the pod"))
-			},
-			DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
-				return nil
-			},
-		},
-		expectedErr:  errors.NewInternalError(fmt.Errorf("failed to get the pod")),
-		expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [ERROR\nInternal error occurred: failed to get the pod]"},
-	}, {
-		name:   "failed delete",
-		object: pod.DeepCopy(),
-		client: &tclient.FakeClient{
-			GetFn: func(_ context.Context, _ int, _ ctrlclient.ObjectKey, _ ctrlclient.Object, _ ...ctrlclient.GetOption) error {
-				return nil
-			},
-			DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
-				return errors.NewInternalError(fmt.Errorf("failed to delete the pod"))
-			},
-		},
-		expectedErr:  errors.NewInternalError(fmt.Errorf("failed to delete the pod")),
-		expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [ERROR\nInternal error occurred: failed to delete the pod]"},
-	}, {
-		name:   "ok",
-		object: pod.DeepCopy(),
-		client: &tclient.FakeClient{
-			GetFn: func(_ context.Context, call int, key ctrlclient.ObjectKey, obj ctrlclient.Object, _ ...ctrlclient.GetOption) error {
-				if call < 10 {
+	}{
+		{
+			name:   "not found",
+			object: pod,
+			client: &tclient.FakeClient{
+				GetFn: func(_ context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, _ ...ctrlclient.GetOption) error {
+					return errors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), key.Name)
+				},
+				DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
 					return nil
-				}
-				return errors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), key.Name)
+				},
 			},
-			DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
-				return nil
-			},
+			expectedErr:  nil,
+			expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [DONE]"},
 		},
-		expectedErr:  nil,
-		expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [DONE]"},
-	}}
+		{
+			name:   "failed get",
+			object: pod.DeepCopy(),
+			client: &tclient.FakeClient{
+				GetFn: func(_ context.Context, _ int, _ ctrlclient.ObjectKey, _ ctrlclient.Object, _ ...ctrlclient.GetOption) error {
+					return errors.NewInternalError(fmt.Errorf("failed to get the pod"))
+				},
+				DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
+					return nil
+				},
+			},
+			expectedErr:  errors.NewInternalError(fmt.Errorf("failed to get the pod")),
+			expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [ERROR\nInternal error occurred: failed to get the pod]"},
+		},
+		{
+			name:   "failed delete",
+			object: pod.DeepCopy(),
+			client: &tclient.FakeClient{
+				GetFn: func(_ context.Context, _ int, _ ctrlclient.ObjectKey, _ ctrlclient.Object, _ ...ctrlclient.GetOption) error {
+					return nil
+				},
+				DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
+					return errors.NewInternalError(fmt.Errorf("failed to delete the pod"))
+				},
+			},
+			expectedErr:  errors.NewInternalError(fmt.Errorf("failed to delete the pod")),
+			expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [ERROR\nInternal error occurred: failed to delete the pod]"},
+		},
+		{
+			name:   "ok",
+			object: pod.DeepCopy(),
+			client: &tclient.FakeClient{
+				GetFn: func(_ context.Context, call int, key ctrlclient.ObjectKey, obj ctrlclient.Object, _ ...ctrlclient.GetOption) error {
+					if call < 10 {
+						return nil
+					}
+					return errors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), key.Name)
+				},
+				DeleteFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
+					return nil
+				},
+			},
+			expectedErr:  nil,
+			expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [DONE]"},
+		},
+		{
+			name:   "poll succeeds but returns error after",
+			object: pod.DeepCopy(),
+			client: &tclient.FakeClient{
+				GetFn: func(ctx context.Context, call int, key ctrlclient.ObjectKey, obj ctrlclient.Object, _ ...ctrlclient.GetOption) error {
+					return nil
+				},
+				DeleteFn: func(ctx context.Context, call int, obj ctrlclient.Object, _ ...ctrlclient.DeleteOption) error {
+					return nil
+				},
+			},
+			expectedErr:  context.DeadlineExceeded,
+			expectedLogs: []string{"DELETE: [RUNNING...]", "DELETE: [ERROR\ncontext deadline exceeded]"},
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			logger := &tlogging.FakeLogger{}
-			ctx := logging.IntoContext(context.TODO(), logger)
+			ctxt, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			ctx := logging.IntoContext(ctxt, logger)
 			err := operationDelete(ctx, tt.object, tt.client)
 			if tt.expectedErr != nil {
 				assert.EqualError(t, err, tt.expectedErr.Error())
