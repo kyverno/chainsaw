aa7ab439f70b3f60a57a628b45831aa439db3840
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-07 03:02:24 +0530

    100% create
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/create_test.go
+++ pkg/runner/operations/create_test.go
@@ -5,7 +5,9 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/kyverno/chainsaw/pkg/client"
 	tclient "github.com/kyverno/chainsaw/pkg/client/testing"
+	"github.com/kyverno/chainsaw/pkg/runner/cleanup"
 	"github.com/kyverno/chainsaw/pkg/runner/logging"
 	tlogging "github.com/kyverno/chainsaw/pkg/runner/logging/testing"
 	"github.com/stretchr/testify/assert"
@@ -15,6 +17,10 @@ import (
 )
 
 func Test_create(t *testing.T) {
+	var cleanerCalled bool
+	testCleaner := func(obj ctrlclient.Object, c client.Client) {
+		cleanerCalled = true
+	}
 	pod := &unstructured.Unstructured{
 		Object: map[string]interface{}{
 			"apiVersion": "v1",
@@ -36,6 +42,7 @@ func Test_create(t *testing.T) {
 		name        string
 		object      ctrlclient.Object
 		client      *tclient.FakeClient
+		cleaner     cleanup.Cleaner
 		shouldFail  bool
 		expectedErr error
 	}{{
@@ -98,16 +105,49 @@ func Test_create(t *testing.T) {
 		},
 		shouldFail:  true,
 		expectedErr: nil,
-	}}
+	},
+		{
+			name:   "Cleaner function executed",
+			object: pod,
+			client: &tclient.FakeClient{
+				GetFn: func(ctx context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
+					return kerrors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), key.Name)
+				},
+				CreateFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.CreateOption) error {
+					return nil
+				},
+			},
+			shouldFail:  false,
+			expectedErr: nil,
+			cleaner:     testCleaner,
+		},
+		{
+			name:   "Should fail is true but no error occurs",
+			object: pod,
+			client: &tclient.FakeClient{
+				GetFn: func(ctx context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
+					return kerrors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), key.Name)
+				},
+				CreateFn: func(_ context.Context, _ int, _ ctrlclient.Object, _ ...ctrlclient.CreateOption) error {
+					return nil
+				},
+			},
+			shouldFail:  true,
+			expectedErr: errors.New("an error was expected but didn't happen"),
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			logger := &tlogging.FakeLogger{}
 			ctx := logging.IntoContext(context.TODO(), logger)
-			err := operationCreate(ctx, tt.object, tt.client, tt.shouldFail, nil)
+			err := operationCreate(ctx, tt.object, tt.client, tt.shouldFail, tt.cleaner)
 			if tt.expectedErr != nil {
 				assert.EqualError(t, err, tt.expectedErr.Error())
 			} else {
 				assert.NoError(t, err)
+			}
+			if tt.cleaner != nil {
+				assert.True(t, cleanerCalled, "cleaner was not called when expected")
 			}
 		})
 	}
