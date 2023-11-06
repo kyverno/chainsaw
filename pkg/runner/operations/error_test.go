14c11d41d9323ea7ab364b680e0d7f4f6eb11f7b
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-07 03:38:26 +0530

    error_test.go
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/error_test.go
+++ pkg/runner/operations/error_test.go
@@ -32,38 +32,74 @@ func Test_operationError(t *testing.T) {
 		client       *tclient.FakeClient
 		expectedErr  error
 		expectedLogs []string
-	}{{
-		name:     "Resource not found",
-		expected: expected,
-		client: &tclient.FakeClient{
-			ListFn: func(ctx context.Context, _ int, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error {
-				return errors.NewNotFound(list.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), "test-pod")
+	}{
+		{
+			name:     "Resource not found",
+			expected: expected,
+			client: &tclient.FakeClient{
+				ListFn: func(ctx context.Context, _ int, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error {
+					return errors.NewNotFound(list.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), "test-pod")
+				},
+				GetFn: func(ctx context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
+					return errors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), "test-pod")
+				},
 			},
-			GetFn: func(ctx context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
-				return errors.NewNotFound(obj.GetObjectKind().GroupVersionKind().GroupVersion().WithResource("pod").GroupResource(), "test-pod")
-			},
+			expectedErr:  nil,
+			expectedLogs: []string{"ERROR : [RUNNING...]", "ERROR : [DONE]"},
 		},
-		expectedErr:  nil,
-		expectedLogs: []string{"ERROR : [RUNNING...]", "ERROR : [DONE]"},
-	}, {
-		name:     "Resource matches actual",
-		expected: expected,
-		client: &tclient.FakeClient{
-			ListFn: func(ctx context.Context, _ int, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error {
-				return nil
+		{
+			name:     "Resource matches actual",
+			expected: expected,
+			client: &tclient.FakeClient{
+				ListFn: func(ctx context.Context, _ int, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error {
+					return nil
+				},
+				GetFn: func(ctx context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
+					uObj, ok := obj.(*unstructured.Unstructured)
+					if !ok {
+						t.Fatalf("obj is not of type *unstructured.Unstructured, it's %T", obj)
+					}
+					uObj.Object = expected.Object
+					return nil
+				},
 			},
-			GetFn: func(ctx context.Context, _ int, key ctrlclient.ObjectKey, obj ctrlclient.Object, opts ...ctrlclient.GetOption) error {
-				uObj, ok := obj.(*unstructured.Unstructured)
-				if !ok {
-					t.Fatalf("obj is not of type *unstructured.Unstructured, it's %T", obj)
-				}
-				uObj.Object = expected.Object
-				return nil
-			},
+			expectedErr:  fmt.Errorf("found an actual resource matching expectation (v1/Pod / foo/test-pod)"),
+			expectedLogs: []string{"ERROR : [RUNNING...]", "ERROR : [ERROR\nfound an actual resource matching expectation (v1/Pod / foo/test-pod)]"},
 		},
-		expectedErr:  fmt.Errorf("found an actual resource matching expectation (v1/Pod / foo/test-pod)"),
-		expectedLogs: []string{"ERROR : [RUNNING...]", "ERROR : [ERROR\nfound an actual resource matching expectation (v1/Pod / foo/test-pod)]"},
-	}}
+		{
+			name: "No resources found using List",
+			expected: unstructured.Unstructured{
+				Object: map[string]interface{}{
+					"apiVersion": "v1",
+					"kind":       "Pod",
+					"metadata": map[string]interface{}{
+						"namespace": "test-ns",
+						"labels": map[string]interface{}{
+							"app": "my-app",
+						},
+					},
+					"spec": map[string]interface{}{
+						"containers": []interface{}{
+							map[string]interface{}{
+								"name":  "test-container",
+								"image": "test-image",
+							},
+						},
+					},
+				},
+			},
+			client: &tclient.FakeClient{
+				ListFn: func(ctx context.Context, _ int, list ctrlclient.ObjectList, opts ...ctrlclient.ListOption) error {
+					t.Helper()
+					uList := list.(*unstructured.UnstructuredList)
+					uList.Items = nil
+					return nil
+				},
+			},
+			expectedErr:  nil,
+			expectedLogs: []string{"ERROR : [RUNNING...]", "ERROR : [DONE]"},
+		},
+	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			logger := &tlogging.FakeLogger{}
