cfd1954db4023372c6fef3de5f523918ee0abc52
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-14 17:27:59 +0530

    fix test
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/apply_test.go
+++ pkg/runner/operations/apply_test.go
@@ -246,10 +246,11 @@ func Test_apply(t *testing.T) {
 			ctx := logging.IntoContext(context.TODO(), logger)
 			operationApply := &ApplyOperation{
 				BaseOperation: BaseOperation{
-					client:  tt.client,
-					obj:     tt.object,
-					dryRun:  tt.dryRun,
-					cleaner: tt.cleaner,
+					client:     tt.client,
+					obj:        tt.object,
+					dryRun:     tt.dryRun,
+					cleaner:    tt.cleaner,
+					shouldFail: tt.shouldFail,
 				},
 			}
 			err := execOperation(ctx, operationApply)
