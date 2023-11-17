12cdaeb6f151869e6d4e7986860ff575fb2b3d53
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-17 21:07:54 +0530

    Add delete refactor
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/delete/delete.go
+++ pkg/runner/operations/delete/delete.go
@@ -9,7 +9,7 @@ import (
 	"github.com/kyverno/chainsaw/pkg/runner/operations"
 	"github.com/kyverno/chainsaw/pkg/runner/operations/internal"
 	"github.com/kyverno/kyverno/ext/output/color"
-	"k8s.io/apimachinery/pkg/api/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/util/wait"
 	ctrlclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -29,50 +29,63 @@ func New(client client.Client, obj ctrlclient.Object, namespacer namespacer.Name
 	}
 }
 
-func (o *operation) Exec(ctx context.Context) (_err error) {
+func (o *operation) Exec(ctx context.Context) error {
 	logger := logging.FromContext(ctx).WithResource(o.obj)
-	defer func() {
-		if _err == nil {
-			logger.Log(logging.Delete, logging.DoneStatus, color.BoldGreen)
-		} else {
-			logger.Log(logging.Delete, logging.ErrorStatus, color.BoldRed, logging.ErrSection(_err))
-		}
-	}()
+
 	if o.namespacer != nil {
 		if err := o.namespacer.Apply(o.obj); err != nil {
 			return err
 		}
 	}
+
 	logger.Log(logging.Delete, logging.RunStatus, color.BoldFgCyan)
-	candidates, _err := internal.Read(ctx, o.obj, o.client)
-	if _err != nil {
-		if errors.IsNotFound(_err) {
+
+	return o.deleteResource(ctx, logger)
+}
+
+func (o *operation) deleteResource(ctx context.Context, logger logging.Logger) error {
+	candidates, err := internal.Read(ctx, o.obj, o.client)
+	if err != nil {
+		if kerrors.IsNotFound(err) {
+			logger.Log(logging.Delete, logging.DoneStatus, color.BoldGreen)
 			return nil
 		}
-		return _err
+		logger.Log(logging.Delete, logging.ErrorStatus, color.BoldRed, logging.ErrSection(err))
+		return err
 	}
-	for i := range candidates {
-		err := o.client.Delete(ctx, &candidates[i])
-		if err != nil && !errors.IsNotFound(err) {
+
+	for _, candidate := range candidates {
+		if err := o.tryDeleteCandidate(ctx, &candidate); err != nil {
+			logger.Log(logging.Delete, logging.ErrorStatus, color.BoldRed, logging.ErrSection(err))
+			return err
+		}
+		if err := o.waitForDeletion(ctx, &candidate); err != nil {
+			logger.Log(logging.Delete, logging.ErrorStatus, color.BoldRed, logging.ErrSection(err))
 			return err
 		}
 	}
-	gvk := o.obj.GetObjectKind().GroupVersionKind()
-	for i := range candidates {
-		if err := wait.PollUntilContextCancel(ctx, internal.PollInterval, true, func(ctx context.Context) (bool, error) {
-			var actual unstructured.Unstructured
-			actual.SetGroupVersionKind(gvk)
-			err := o.client.Get(ctx, client.ObjectKey(&candidates[i]), &actual)
-			if err != nil {
-				if errors.IsNotFound(err) {
-					return true, nil
-				}
-				return false, err
-			}
-			return false, nil
-		}); err != nil {
-			return err
-		}
+
+	logger.Log(logging.Delete, logging.DoneStatus, color.BoldGreen)
+	return nil
+}
+
+func (o *operation) tryDeleteCandidate(ctx context.Context, candidate *unstructured.Unstructured) error {
+	err := o.client.Delete(ctx, candidate)
+	if err != nil && !kerrors.IsNotFound(err) {
+		return err
 	}
 	return nil
+}
+
+func (o *operation) waitForDeletion(ctx context.Context, candidate *unstructured.Unstructured) error {
+	gvk := candidate.GetObjectKind().GroupVersionKind()
+	return wait.PollUntilContextCancel(ctx, internal.PollInterval, true, func(ctx context.Context) (bool, error) {
+		var actual unstructured.Unstructured
+		actual.SetGroupVersionKind(gvk)
+		err := o.client.Get(ctx, client.ObjectKey(candidate), &actual)
+		if kerrors.IsNotFound(err) {
+			return true, nil
+		}
+		return false, err
+	})
 }
