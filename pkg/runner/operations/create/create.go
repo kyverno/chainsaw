0b2de0fb8551717aa1bbe388d8be80c47035b977
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-17 20:04:04 +0530

    clear create code
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/create/create.go
+++ pkg/runner/operations/create/create.go
@@ -36,50 +36,70 @@ func New(client client.Client, obj ctrlclient.Object, namespacer namespacer.Name
 	}
 }
 
-func (o *operation) Exec(ctx context.Context) (_err error) {
+func (o *operation) Exec(ctx context.Context) error {
 	logger := logging.FromContext(ctx).WithResource(o.obj)
-	defer func() {
-		if _err == nil {
-			logger.Log(logging.Create, logging.DoneStatus, color.BoldGreen)
-		} else {
-			logger.Log(logging.Create, logging.ErrorStatus, color.BoldRed, logging.ErrSection(_err))
-		}
-	}()
+
 	if o.namespacer != nil {
 		if err := o.namespacer.Apply(o.obj); err != nil {
 			return err
 		}
 	}
+
 	logger.Log(logging.Create, logging.RunStatus, color.BoldFgCyan)
-	return wait.PollUntilContextCancel(ctx, internal.PollInterval, false, func(ctx context.Context) (bool, error) {
-		var actual unstructured.Unstructured
-		actual.SetGroupVersionKind(o.obj.GetObjectKind().GroupVersionKind())
-		err := o.client.Get(ctx, client.ObjectKey(o.obj), &actual)
-		if err == nil {
-			return false, errors.New("the resource already exists in the cluster")
-		} else if kerrors.IsNotFound(err) {
-			err := o.client.Create(ctx, o.obj)
-			if err == nil && o.cleaner != nil {
-				o.cleaner(o.obj, o.client)
-			}
-			if o.check == nil {
-				return err == nil, err
-			} else {
-				actual := map[string]interface{}{
-					"error":    nil,
-					"resource": o.obj,
-				}
-				if err != nil {
-					actual["error"] = err.Error()
-				}
-				errs, err := assert.Validate(ctx, o.check, actual, nil)
-				if err != nil {
-					return false, err
-				}
-				return true, errs.ToAggregate()
-			}
-		} else {
-			return false, err
-		}
+
+	return o.createResource(ctx, logger)
+}
+
+func (o *operation) createResource(ctx context.Context, logger logging.Logger) error {
+	err := wait.PollUntilContextCancel(ctx, internal.PollInterval, false, func(ctx context.Context) (bool, error) {
+		err := o.tryCreateResource(ctx)
+		return err == nil, err
 	})
+	if err != nil {
+		logger.Log(logging.Create, logging.ErrorStatus, color.BoldRed, logging.ErrSection(err))
+		return err
+	}
+
+	logger.Log(logging.Create, logging.DoneStatus, color.BoldGreen)
+	return nil
+}
+
+func (o *operation) tryCreateResource(ctx context.Context) error {
+	var actual unstructured.Unstructured
+	actual.SetGroupVersionKind(o.obj.GetObjectKind().GroupVersionKind())
+	err := o.client.Get(ctx, client.ObjectKey(o.obj), &actual)
+	if err == nil {
+		return errors.New("the resource already exists in the cluster")
+	} else if kerrors.IsNotFound(err) {
+		return o.create_Resource(ctx)
+	}
+	return err
+}
+
+func (o *operation) create_Resource(ctx context.Context) error {
+	err := o.client.Create(ctx, o.obj)
+	if err == nil && o.cleaner != nil {
+		o.cleaner(o.obj, o.client)
+	}
+	return o.handleCheck(ctx, err)
+}
+
+func (o *operation) handleCheck(ctx context.Context, err error) error {
+	if o.check == nil {
+		return err
+	}
+
+	actual := map[string]interface{}{
+		"error":    nil,
+		"resource": o.obj,
+	}
+	if err != nil {
+		actual["error"] = err.Error()
+	}
+
+	errs, validationErr := assert.Validate(ctx, o.check, actual, nil)
+	if validationErr != nil {
+		return validationErr
+	}
+	return errs.ToAggregate()
 }
