afaafd2c580aa464bd973c5acb25c4cf28fe030c
Shubham Gupta <69793468+shubham-cmyk@users.noreply.github.com>
2023-11-25 00:25:56 +0530

    pkg/validation Part2 (#488)
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

---
+++ pkg/validation/test_test.go
@@ -0,0 +1,88 @@
+package validation
+
+import (
+	"testing"
+
+	v1alpha1 "github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidateTest(t *testing.T) {
+	validTestSpec := v1alpha1.TestSpec{
+		Steps: []v1alpha1.TestSpecStep{
+			{
+				Name: "step1",
+				Spec: v1alpha1.TestStepSpec{
+					Try: []v1alpha1.Operation{
+						{
+							Apply: &v1alpha1.Apply{
+								FileRefOrResource: v1alpha1.FileRefOrResource{
+									FileRef: v1alpha1.FileRef{
+										File: "file",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	invalidTestSpec := v1alpha1.TestSpec{
+		Steps: []v1alpha1.TestSpecStep{
+			{
+				Name: "step1",
+				Spec: v1alpha1.TestStepSpec{
+					Try: []v1alpha1.Operation{
+						{
+							Apply: &v1alpha1.Apply{
+								FileRefOrResource: v1alpha1.FileRefOrResource{
+									FileRef: v1alpha1.FileRef{
+										File: "file",
+									},
+								},
+							},
+							Assert: &v1alpha1.Assert{
+								FileRef: v1alpha1.FileRef{
+									File: "file",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		name      string
+		input     *v1alpha1.Test
+		expectErr bool
+	}{
+		{
+			name: "Valid TestSpec",
+			input: &v1alpha1.Test{
+				Spec: validTestSpec,
+			},
+			expectErr: false,
+		},
+		{
+			name: "Invalid TestSpec",
+			input: &v1alpha1.Test{
+				Spec: invalidTestSpec,
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			errs := ValidateTest(tt.input)
+			if tt.expectErr {
+				assert.NotEmpty(t, errs)
+			} else {
+				assert.Empty(t, errs)
+			}
+		})
+	}
+}
