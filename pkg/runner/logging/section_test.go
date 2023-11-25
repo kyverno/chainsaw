da09509d8fd1ae91c4e539edbbab23a2bca2f17d
Shubham Gupta <69793468+shubham-cmyk@users.noreply.github.com>
2023-11-26 02:35:36 +0530

    section (#501)
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

---
+++ pkg/runner/logging/section_test.go
@@ -0,0 +1,60 @@
+package logging
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/multierr"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+)
+
+func TestSection(t *testing.T) {
+	tests := []struct {
+		name     string
+		args     []interface{}
+		expected string
+	}{
+		{"empty", nil, "=== EMPTY\n"},
+		{"single arg", []interface{}{"example"}, "=== SINGLE ARG\nexample"},
+		{"multiple args", []interface{}{"one", 2, 3.0}, "=== MULTIPLE ARGS\none2 3"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sec := Section(tt.name, tt.args...)
+			assert.Equal(t, tt.expected, sec.String(), "Output should match expected string")
+		})
+	}
+}
+
+func TestErrSection(t *testing.T) {
+	tests := []struct {
+		name     string
+		err      error
+		expected string
+	}{
+		{
+			"single error",
+			errors.New("error 1"),
+			"=== ERROR\nerror 1",
+		},
+		{
+			"multi error",
+			multierr.Combine(errors.New("error 1"), errors.New("error 2")),
+			"=== ERROR\nerror 1\nerror 2",
+		},
+		{
+			"aggregate error",
+			utilerrors.NewAggregate([]error{errors.New("error 1"), errors.New("error 2")}),
+			"=== ERROR\nerror 1\nerror 2",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sec := ErrSection(tt.err)
+			assert.Equal(t, tt.expected, sec.String(), "Output should match expected string")
+		})
+	}
+}
