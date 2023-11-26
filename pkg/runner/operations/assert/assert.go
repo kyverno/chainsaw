8682cf1d1339e95878ad9e81a297bac866086a4b
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-11-27 01:34:33 +0530

    remove comment
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- pkg/runner/operations/assert/assert.go
+++ pkg/runner/operations/assert/assert.go
@@ -33,69 +33,6 @@ func New(client client.Client, expected unstructured.Unstructured, namespacer na
 	}
 }
 
-// func (o *operation) Exec(ctx context.Context) (_err error) {
-// 	logger := logging.FromContext(ctx).WithResource(&o.expected)
-// 	defer func() {
-// 		if _err == nil {
-// 			logger.Log(logging.Assert, logging.DoneStatus, color.BoldGreen)
-// 		} else {
-// 			logger.Log(logging.Assert, logging.ErrorStatus, color.BoldRed, logging.ErrSection(_err))
-// 		}
-// 	}()
-// 	if o.namespacer != nil {
-// 		if err := o.namespacer.Apply(&o.expected); err != nil {
-// 			return err
-// 		}
-// 	}
-// 	logger.Log(logging.Assert, logging.RunStatus, color.BoldFgCyan)
-// 	var lastErrs []error
-// 	err := wait.PollUntilContextCancel(ctx, internal.PollInterval, false, func(ctx context.Context) (_ bool, err error) {
-// 		var errs []error
-// 		defer func() {
-// 			// record last errors only if there was no real error
-// 			if err == nil {
-// 				lastErrs = errs
-// 			}
-// 		}()
-// 		if candidates, err := internal.Read(ctx, &o.expected, o.client); err != nil {
-// 			if kerrors.IsNotFound(err) {
-// 				errs = append(errs, errors.New("actual resource not found"))
-// 				return false, nil
-// 			}
-// 			return false, err
-// 		} else if len(candidates) == 0 {
-// 			errs = append(errs, errors.New("no actual resource found"))
-// 		} else {
-// 			for i := range candidates {
-// 				candidate := candidates[i]
-// 				_errs, err := assert.Validate(ctx, o.expected.UnstructuredContent(), candidate.UnstructuredContent(), nil)
-// 				if err != nil {
-// 					return false, err
-// 				}
-// 				if len(_errs) != 0 {
-// 					for _, _err := range _errs {
-// 						errs = append(errs, fmt.Errorf("%s/%s/%s - %w", candidate.GetAPIVersion(), candidate.GetKind(), client.Name(client.ObjectKey(&candidate)), _err))
-// 					}
-// 				} else {
-// 					// at least one match found
-// 					return true, nil
-// 				}
-// 			}
-// 		}
-// 		return false, nil
-// 	})
-// 	// if no error, return success
-// 	if err == nil {
-// 		return nil
-// 	}
-// 	// eventually return a combination of last errors
-// 	if len(lastErrs) != 0 {
-// 		return multierr.Combine(lastErrs...)
-// 	}
-// 	// return received error
-// 	return err
-// }
-
 func (o *operation) Exec(ctx context.Context) (_err error) {
 	logger := logging.FromContext(ctx).WithResource(&o.expected)
 	defer func() {
