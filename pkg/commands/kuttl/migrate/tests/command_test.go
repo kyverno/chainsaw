cc1091e6419073899b80268db77a37a82799ceda
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-11-30 10:33:38 +0100

    chore: add unit tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/kuttl/migrate/tests/command_test.go
+++ pkg/commands/kuttl/migrate/tests/command_test.go
@@ -50,6 +50,13 @@ func Test_Execute(t *testing.T) {
 			"--foo",
 		},
 		wantErr: true,
+	}, {
+		name: "bad folder",
+		args: []string{
+			"tests",
+			"/home/",
+		},
+		wantErr: true,
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
