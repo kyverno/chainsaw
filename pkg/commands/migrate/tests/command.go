5c3f31d2b9efc5295a7d0ec4cb9f51c77f32d0f6
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-12-07 21:47:12 +0100

    fix: migration commands
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/migrate/tests/command.go
+++ pkg/commands/migrate/tests/command.go
@@ -110,7 +110,7 @@ func processFolder(out io.Writer, folder string, save, cleanup bool) error {
 }
 
 func processStep(out io.Writer, step *v1alpha1.TestSpecStep, s discovery.Step, folder string, save bool) error {
-	for _, file := range s.OtherFiles {
+	for f, file := range s.OtherFiles {
 		resources, err := resource.Load(filepath.Join(folder, file))
 		if err != nil {
 			return err
@@ -128,7 +128,7 @@ func processStep(out io.Writer, step *v1alpha1.TestSpecStep, s discovery.Step, f
 					return fmt.Errorf("type not supported %s / %s", resource.GetAPIVersion(), resource.GetKind())
 				}
 			} else {
-				file := fmt.Sprintf("chainsaw-%s-apply-%d.yaml", step.Name, i+1)
+				file := fmt.Sprintf("chainsaw-%s-apply-%d-%d.yaml", step.Name, f+1, i+1)
 				if save {
 					if err := saveResource(out, folder, file, resource); err != nil {
 						return err
@@ -146,7 +146,7 @@ func processStep(out io.Writer, step *v1alpha1.TestSpecStep, s discovery.Step, f
 			}
 		}
 	}
-	for _, file := range s.AssertFiles {
+	for f, file := range s.AssertFiles {
 		resources, err := resource.Load(filepath.Join(folder, file))
 		if err != nil {
 			return err
@@ -158,7 +158,7 @@ func processStep(out io.Writer, step *v1alpha1.TestSpecStep, s discovery.Step, f
 					return fmt.Errorf("type not supported %s / %s", resource.GetAPIVersion(), resource.GetKind())
 				}
 			} else {
-				file := fmt.Sprintf("chainsaw-%s-assert-%d.yaml", step.Name, i+1)
+				file := fmt.Sprintf("chainsaw-%s-assert-%d-%d.yaml", step.Name, f+1, i+1)
 				if save {
 					if err := saveResource(out, folder, file, resource); err != nil {
 						return err
@@ -176,7 +176,7 @@ func processStep(out io.Writer, step *v1alpha1.TestSpecStep, s discovery.Step, f
 			}
 		}
 	}
-	for _, file := range s.ErrorFiles {
+	for f, file := range s.ErrorFiles {
 		resources, err := resource.Load(filepath.Join(folder, file))
 		if err != nil {
 			return err
@@ -188,7 +188,7 @@ func processStep(out io.Writer, step *v1alpha1.TestSpecStep, s discovery.Step, f
 					return fmt.Errorf("type not supported %s / %s", resource.GetAPIVersion(), resource.GetKind())
 				}
 			} else {
-				file := fmt.Sprintf("chainsaw-%s-error-%d.yaml", step.Name, i+1)
+				file := fmt.Sprintf("chainsaw-%s-error-%d-%d.yaml", step.Name, f+1, i+1)
 				if save {
 					if err := saveResource(out, folder, file, resource); err != nil {
 						return err
