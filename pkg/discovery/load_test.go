482597c191c9f440b89e9488a08519125b123a6e
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-12-07 10:07:37 +0100

    fix tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/discovery/load_test.go
+++ pkg/discovery/load_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/utils/ptr"
 )
 
 func TestLoadTest(t *testing.T) {
@@ -19,369 +18,128 @@ func TestLoadTest(t *testing.T) {
 		path     string
 		want     *Test
 		wantErr  bool
-	}{
-		{
-			name:     "invalid path",
-			fileName: "chainsaw-test.yaml",
-			path:     "/invalid",
-			want:     nil,
-			wantErr:  true,
-		},
-		{
-			name:     "no path",
-			fileName: "chainsaw-test.yaml",
-			path:     "",
-			want:     nil,
-			wantErr:  true,
-		},
-		{
-			name:     "test",
-			fileName: "chainsaw-test.yaml",
-			path:     filepath.Join(basePath, "test"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/test",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-					Spec: v1alpha1.TestSpec{
-						Steps: []v1alpha1.TestSpecStep{{
-							Name: "create configmap",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{
-									{
-										Apply: &v1alpha1.Apply{
-											FileRefOrResource: v1alpha1.FileRefOrResource{
-												FileRef: v1alpha1.FileRef{
-													File: "configmap.yaml",
-												},
-											},
-										},
-									},
-								},
-							},
-						}, {
-							Name: "assert configmap",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{{
-									Assert: &v1alpha1.Assert{
+	}{{
+		name:     "invalid path",
+		fileName: "chainsaw-test.yaml",
+		path:     "/invalid",
+		want:     nil,
+		wantErr:  true,
+	}, {
+		name:     "no path",
+		fileName: "chainsaw-test.yaml",
+		path:     "",
+		want:     nil,
+		wantErr:  true,
+	}, {
+		name:     "test",
+		fileName: "chainsaw-test.yaml",
+		path:     filepath.Join(basePath, "test"),
+		want: &Test{
+			BasePath: "../../testdata/discovery/test",
+			Test: &v1alpha1.Test{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "chainsaw.kyverno.io/v1alpha1",
+					Kind:       "Test",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "test",
+				},
+				Spec: v1alpha1.TestSpec{
+					Steps: []v1alpha1.TestSpecStep{{
+						Name: "create configmap",
+						TestStepSpec: v1alpha1.TestStepSpec{
+							Try: []v1alpha1.Operation{
+								{
+									Apply: &v1alpha1.Apply{
 										FileRefOrResource: v1alpha1.FileRefOrResource{
 											FileRef: v1alpha1.FileRef{
 												File: "configmap.yaml",
 											},
 										},
 									},
-								}},
+								},
 							},
-						}},
-					},
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name:     "test",
-			fileName: "chainsaw-test.yaml",
-			path:     filepath.Join(basePath, "invalid-test-and-steps"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/invalid-test-and-steps",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-					Spec: v1alpha1.TestSpec{
-						Steps: []v1alpha1.TestSpecStep{{
-							Name: "create configmap",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{
-									{
-										Apply: &v1alpha1.Apply{
-											FileRefOrResource: v1alpha1.FileRefOrResource{
-												FileRef: v1alpha1.FileRef{
-													File: "configmap.yaml",
-												},
-											},
+						},
+					}, {
+						Name: "assert configmap",
+						TestStepSpec: v1alpha1.TestStepSpec{
+							Try: []v1alpha1.Operation{{
+								Assert: &v1alpha1.Assert{
+									FileRefOrResource: v1alpha1.FileRefOrResource{
+										FileRef: v1alpha1.FileRef{
+											File: "configmap.yaml",
 										},
 									},
 								},
-							},
-						}, {
-							Name: "assert configmap",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{
-									{
-										Assert: &v1alpha1.Assert{
-											FileRefOrResource: v1alpha1.FileRefOrResource{
-												FileRef: v1alpha1.FileRef{
-													File: "configmap.yaml",
-												},
-											},
+							}},
+						},
+					}},
+				},
+			},
+		},
+		wantErr: false,
+	}, {
+		name:     "manifests",
+		fileName: "chainsaw-test.yaml",
+		path:     filepath.Join(basePath, "manifests"),
+		want: &Test{
+			BasePath: "../../testdata/discovery/manifests",
+			Test: &v1alpha1.Test{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "chainsaw.kyverno.io/v1alpha1",
+					Kind:       "Test",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "manifests",
+				},
+				Spec: v1alpha1.TestSpec{
+					Steps: []v1alpha1.TestSpecStep{{
+						Name: "step-01",
+						TestStepSpec: v1alpha1.TestStepSpec{
+							Try: []v1alpha1.Operation{{
+								Apply: &v1alpha1.Apply{
+									FileRefOrResource: v1alpha1.FileRefOrResource{
+										FileRef: v1alpha1.FileRef{
+											File: "01-configmap.yaml",
 										},
 									},
 								},
-							},
-						}},
-					},
+							}, {
+								Assert: &v1alpha1.Assert{
+									FileRefOrResource: v1alpha1.FileRefOrResource{
+										FileRef: v1alpha1.FileRef{
+											File: "01-assert.yaml",
+										},
+									},
+								},
+							}, {
+								Error: &v1alpha1.Error{
+									FileRefOrResource: v1alpha1.FileRefOrResource{
+										FileRef: v1alpha1.FileRef{
+											File: "01-errors.yaml",
+										},
+									},
+								},
+							}},
+						},
+					}},
 				},
 			},
-			wantErr: false,
 		},
-		{
-			name:     "test",
-			fileName: "chainsaw-test.yaml",
-			path:     filepath.Join(basePath, "test-and-steps"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/test-and-steps",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "test",
-					},
-					Spec: v1alpha1.TestSpec{
-						Skip:       ptr.To(false),
-						Concurrent: ptr.To(false),
-						Steps: []v1alpha1.TestSpecStep{{
-							Name: "test-1",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{{
-									Apply: &v1alpha1.Apply{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "foo.yaml",
-											},
-										},
-									},
-								}, {
-									Assert: &v1alpha1.Assert{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "bar.yaml",
-											},
-										},
-									},
-								}},
-							},
-						}},
-					},
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name:     "steps",
-			fileName: "chainsaw-test.yaml",
-			path:     filepath.Join(basePath, "steps"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/steps",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "steps",
-					},
-					Spec: v1alpha1.TestSpec{
-						Steps: []v1alpha1.TestSpecStep{{
-							Name: "test-1",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{{
-									Apply: &v1alpha1.Apply{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "foo.yaml",
-											},
-										},
-									},
-								}, {
-									Assert: &v1alpha1.Assert{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "bar.yaml",
-											},
-										},
-									},
-								}},
-							},
-						}},
-					},
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name:     "steps and manifests",
-			fileName: "chainsaw-test.yaml",
-			path:     filepath.Join(basePath, "steps-and-manifests"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/steps-and-manifests",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "steps-and-manifests",
-					},
-					Spec: v1alpha1.TestSpec{
-						Steps: []v1alpha1.TestSpecStep{{
-							Name: "test-1",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{{
-									Apply: &v1alpha1.Apply{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "foo.yaml",
-											},
-										},
-									},
-								}, {
-									Assert: &v1alpha1.Assert{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "bar.yaml",
-											},
-										},
-									},
-								}, {
-									Apply: &v1alpha1.Apply{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "01-configmap.yaml",
-											},
-										},
-									},
-								}, {
-									Assert: &v1alpha1.Assert{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "01-assert.yaml",
-											},
-										},
-									},
-								}, {
-									Error: &v1alpha1.Error{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "01-errors.yaml",
-											},
-										},
-									},
-								}},
-							},
-						}},
-					},
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name:     "manifests",
-			fileName: "chainsaw-test.yaml",
-			path:     filepath.Join(basePath, "manifests"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/manifests",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "manifests",
-					},
-					Spec: v1alpha1.TestSpec{
-						Steps: []v1alpha1.TestSpecStep{{
-							Name: "step-01",
-							TestStepSpec: v1alpha1.TestStepSpec{
-								Try: []v1alpha1.Operation{{
-									Apply: &v1alpha1.Apply{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "01-configmap.yaml",
-											},
-										},
-									},
-								}, {
-									Assert: &v1alpha1.Assert{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "01-assert.yaml",
-											},
-										},
-									},
-								}, {
-									Error: &v1alpha1.Error{
-										FileRefOrResource: v1alpha1.FileRefOrResource{
-											FileRef: v1alpha1.FileRef{
-												File: "01-errors.yaml",
-											},
-										},
-									},
-								}},
-							},
-						}},
-					},
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name:     "empty test",
-			fileName: "",
-			path:     filepath.Join(basePath, "empty-test"),
-			want:     nil,
-			wantErr:  false,
-		},
-		{
-			name:     "multiple tests",
-			fileName: "chainsaw-test.yaml",
-			path:     filepath.Join(basePath, "multiple-tests"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/multiple-tests",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "multiple-tests",
-					},
-					Spec: v1alpha1.TestSpec{},
-				},
-			},
-			wantErr: false,
-		},
-		{
-			name:     "multiple steps",
-			fileName: "",
-			path:     filepath.Join(basePath, "multiple-steps"),
-			want: &Test{
-				BasePath: "../../testdata/discovery/multiple-steps",
-				Test: &v1alpha1.Test{
-					TypeMeta: metav1.TypeMeta{
-						APIVersion: "chainsaw.kyverno.io/v1alpha1",
-						Kind:       "Test",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: "multiple-steps",
-					},
-					Spec: v1alpha1.TestSpec{},
-				},
-			},
-			wantErr: false,
-		},
-	}
+		wantErr: false,
+	}, {
+		name:     "empty test",
+		fileName: "",
+		path:     filepath.Join(basePath, "empty-test"),
+		want:     nil,
+		wantErr:  false,
+	}, {
+		name:     "multiple tests",
+		fileName: "chainsaw-test.yaml",
+		path:     filepath.Join(basePath, "multiple-tests"),
+		want:     nil,
+		wantErr:  true,
+	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := LoadTest(tt.fileName, tt.path)
@@ -395,8 +153,8 @@ func TestLoadTest(t *testing.T) {
 				if got != nil {
 					got.Err = nil
 				}
-				assert.Equal(t, tt.want, got)
 			}
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
