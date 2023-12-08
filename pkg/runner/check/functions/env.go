37f35b2a61580d4160d1ab45ceaa99b72e57baed
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-12-08 23:11:54 +0100

    chore: add a couple unit tests (#627)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/check/functions/env.go
+++ pkg/runner/check/functions/env.go
@@ -2,10 +2,14 @@ package functions
 
 import (
 	"errors"
+	"fmt"
 	"os"
 )
 
 func jpEnv(arguments []any) (any, error) {
+	if len(arguments) != 1 {
+		return nil, fmt.Errorf("invalid number of arguments, expected %d, got %d", 1, len(arguments))
+	}
 	if key, ok := arguments[0].(string); !ok {
 		return nil, errors.New("invalid type, first argument must be a string")
 	} else {
---
+++ pkg/runner/check/functions/env_test.go
@@ -0,0 +1,58 @@
+package functions
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_jpEnv(t *testing.T) {
+	tests := []struct {
+		name      string
+		arguments []any
+		want      any
+		wantErr   bool
+	}{{
+		name:      "nil",
+		arguments: nil,
+		want:      nil,
+		wantErr:   true,
+	}, {
+		name:      "empty",
+		arguments: []any{},
+		want:      nil,
+		wantErr:   true,
+	}, {
+		name:      "two",
+		arguments: []any{"", 1},
+		want:      nil,
+		wantErr:   true,
+	}, {
+		name:      "not found",
+		arguments: []any{"FOO"},
+		want:      "",
+		wantErr:   false,
+	}, {
+		name:      "found",
+		arguments: []any{"BAR"},
+		want:      "some value",
+		wantErr:   false,
+	}, {
+		name:      "wrong type",
+		arguments: []any{12},
+		want:      nil,
+		wantErr:   true,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Setenv("BAR", "some value")
+			got, err := jpEnv(tt.arguments)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
