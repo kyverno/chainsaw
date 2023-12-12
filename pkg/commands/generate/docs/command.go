6a1ef27f1bb511c0baa239f1c3663d0bd48db9c6
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-12-12 23:52:57 +0100

    chore: add a couple unit tests (#660)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/generate/docs/command.go
+++ pkg/commands/generate/docs/command.go
@@ -3,6 +3,7 @@ package docs
 import (
 	_ "embed"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 	"text/template"
@@ -15,9 +16,18 @@ import (
 //go:embed docs.tmpl
 var docsTemplate string
 
+//go:embed catalog.tmpl
+var catalogTemplate string
+
+var (
+	docsTmpl    = template.Must(template.New("docs").Funcs(sprig.FuncMap()).Parse(docsTemplate))
+	catalogTmpl = template.Must(template.New("catalog").Funcs(sprig.FuncMap()).Parse(catalogTemplate))
+)
+
 type options struct {
 	testFile   string
 	readmeFile string
+	catalog    string
 	testDirs   []string
 }
 
@@ -25,31 +35,21 @@ func Command() *cobra.Command {
 	var options options
 	cmd := &cobra.Command{
 		Use:          "docs",
-		Short:        "Generate documentation for tests",
+		Short:        "Generate tests documentation",
 		Args:         cobra.NoArgs,
 		SilenceUsage: true,
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			tmpl, err := template.New("docs").Funcs(sprig.FuncMap()).Parse(docsTemplate)
-			if err != nil {
-				return err
-			}
 			tests, err := discovery.DiscoverTests(options.testFile, options.testDirs...)
 			if err != nil {
 				return err
 			}
-			for _, test := range tests {
-				if test.Err == nil {
-					file, err := os.Create(filepath.Join(test.BasePath, options.readmeFile))
-					if err != nil {
-						return err
-					}
-					defer file.Close()
-					output := file
-					if err := tmpl.Execute(output, test); err != nil {
-						return err
-					}
-				} else {
-					fmt.Fprintf(cmd.OutOrStdout(), "ERROR: failed to load test %s (%s)", test.BasePath, test.Err)
+			out := cmd.OutOrStdout()
+			if err := generateDocs(out, options.readmeFile, tests...); err != nil {
+				return err
+			}
+			if options.catalog != "" {
+				if err := generateCatalog(out, options.readmeFile, options.catalog, tests...); err != nil {
+					return err
 				}
 			}
 			return nil
@@ -57,6 +57,41 @@ func Command() *cobra.Command {
 	}
 	cmd.Flags().StringVar(&options.testFile, "test-file", "chainsaw-test.yaml", "Name of the test file")
 	cmd.Flags().StringVar(&options.readmeFile, "readme-file", "README.md", "Name of the generated docs file")
+	cmd.Flags().StringVar(&options.catalog, "catalog", "", "Path to the generated test catalog file")
 	cmd.Flags().StringArrayVar(&options.testDirs, "test-dir", []string{}, "Directories containing test cases to run")
 	return cmd
+}
+
+func generateDocs(out io.Writer, fileName string, tests ...discovery.Test) error {
+	for _, test := range tests {
+		if test.Err == nil {
+			file, err := os.Create(filepath.Join(test.BasePath, fileName))
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			output := file
+			if err := docsTmpl.Execute(output, test); err != nil {
+				return err
+			}
+		} else {
+			fmt.Fprintf(out, "ERROR: failed to load test %s (%s)", test.BasePath, test.Err)
+		}
+	}
+	return nil
+}
+
+func generateCatalog(out io.Writer, readme string, catalog string, tests ...discovery.Test) error {
+	file, err := os.Create(catalog)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	if err := catalogTmpl.Execute(file, map[string]any{
+		"Readme": readme,
+		"Tests":  tests,
+	}); err != nil {
+		return err
+	}
+	return nil
 }
