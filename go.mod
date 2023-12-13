d47f547ba94acc79d8dcee8a53aeec336b7c3426
Shubham Gupta <iamshubhamgupta2001@gmail.com>
2023-12-14 05:05:19 +0530

    add : lint check
    Signed-off-by: Shubham Gupta <iamshubhamgupta2001@gmail.com>

--- go.mod
+++ go.mod
@@ -106,6 +106,9 @@ require (
 	github.com/smarty/assertions v1.15.1 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	github.com/zach-klippenstein/goregen v0.0.0-20160303162051-795b5e3961ea // indirect
 	go.etcd.io/etcd/api/v3 v3.5.9 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.9 // indirect
--- pkg/commands/lint/command.go
+++ pkg/commands/lint/command.go
@@ -4,32 +4,43 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/xeipuuv/gojsonschema"
 )
+
+func getTestSchema() string {
+	return filepath.Join("..", "..", "..", ".schemas", "json", "test-chainsaw-v1alpha1.json")
+}
+
+func getConfigurationSchema() string {
+	return filepath.Join("..", "..", "..", ".schemas", "json", "configuration-chainsaw-v1alpha1.json")
+}
 
 func Command() *cobra.Command {
 	var fileFlag string
 	var stdInFlag bool
 
 	cmd := &cobra.Command{
-		Use:   "lint",
-		Short: "Lint a file or read from standard input",
-		Long:  `Use chainsaw lint to lint a specific file or read from standard input.`,
+		Use:       "lint [test|configuration]",
+		Short:     "Lint a file or read from standard input",
+		Long:      `Use chainsaw lint to lint a specific file or read from standard input for either test or configuration.`,
+		Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+		ValidArgs: []string{"test", "configuration"},
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if stdInFlag {
 				input, err := io.ReadAll(os.Stdin)
 				if err != nil {
 					return err
 				}
-				return lintInput(input)
+				return lintInput(input, args[0])
 			} else if fileFlag != "" {
 				input, err := os.ReadFile(fileFlag)
 				if err != nil {
 					return err
 				}
-				return lintInput(input)
+				return lintInput(input, args[0])
 			} else {
 				return fmt.Errorf("either --file or --std-in must be specified")
 			}
@@ -42,9 +53,9 @@ func Command() *cobra.Command {
 	return cmd
 }
 
-func lintInput(input []byte) error {
+func lintInput(input []byte, schema string) error {
 	fmt.Println("Processing input...")
-	schemaLoader := gojsonschema.NewReferenceLoader("")
+	schemaLoader := gojsonschema.NewReferenceLoader(getScheme(schema))
 	documentLoader := gojsonschema.NewBytesLoader(input)
 
 	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
@@ -61,4 +72,13 @@ func lintInput(input []byte) error {
 	}
 
 	return nil
+}
+
+func getScheme(schema string) string {
+	switch schema {
+	case "configuration":
+		return getConfigurationSchema()
+	default:
+		return getTestSchema()
+	}
 }
