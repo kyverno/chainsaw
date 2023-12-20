1481d363aa5e80f0a524fec87379ea6cafddabdd
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-12-20 01:54:40 +0100

    schema
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/apis/v1alpha1/file_ref_or_resource.go
+++ pkg/apis/v1alpha1/file_ref_or_resource.go
@@ -11,7 +11,6 @@ type FileRefOrResource struct {
 	FileRef `json:",inline"`
 
 	// Resource provides a resource to be applied.
-	// +kubebuilder:validation:XEmbeddedResource
 	// +kubebuilder:pruning:PreserveUnknownFields
 	// +optional
 	Resource *unstructured.Unstructured `json:"resource,omitempty"`
