a2fcaab233ee85ae81f9fa071eb328951a3714d2
Chandan-DK <chandandk468@gmail.com>
2024-02-16 10:32:55 +0000

    add unit tests for get collector
    Signed-off-by: Chandan-DK <chandandk468@gmail.com>

---
+++ pkg/runner/collect/get_test.go
@@ -0,0 +1,118 @@
+package collect
+
+import (
+	"testing"
+
+	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGet(t *testing.T) {
+	tests := []struct {
+		name      string
+		collector *v1alpha1.Get
+		want      *v1alpha1.Command
+		wantErr   bool
+	}{{
+		name:      "nil",
+		collector: nil,
+		want:      nil,
+		wantErr:   false,
+	}, {
+		name:      "empty",
+		collector: &v1alpha1.Get{},
+		wantErr:   true,
+	}, {
+		name: "without resource",
+		collector: &v1alpha1.Get{
+			Name: "foo",
+		},
+		wantErr: true,
+	}, {
+		name: "with resource",
+		collector: &v1alpha1.Get{
+			Resource: "foos",
+		},
+		want: &v1alpha1.Command{
+			Entrypoint: "kubectl",
+			Args:       []string{"get", "foos", "-n", "$NAMESPACE"},
+		},
+		wantErr: false,
+	}, {
+		name: "with name",
+		collector: &v1alpha1.Get{
+			Resource: "foos",
+			Name:     "foo",
+		},
+		want: &v1alpha1.Command{
+			Entrypoint: "kubectl",
+			Args:       []string{"get", "foos", "foo", "-n", "$NAMESPACE"},
+		},
+		wantErr: false,
+	}, {
+		name: "with namespace",
+		collector: &v1alpha1.Get{
+			Resource:  "foos",
+			Namespace: "bar",
+		},
+		want: &v1alpha1.Command{
+			Entrypoint: "kubectl",
+			Args:       []string{"get", "foos", "-n", "bar"},
+		},
+		wantErr: false,
+	}, {
+		name: "with name and namespace",
+		collector: &v1alpha1.Get{
+			Resource:  "foos",
+			Name:      "foo",
+			Namespace: "bar",
+		},
+		want: &v1alpha1.Command{
+			Entrypoint: "kubectl",
+			Args:       []string{"get", "foos", "foo", "-n", "bar"},
+		},
+		wantErr: false,
+	}, {
+		name: "with selector",
+		collector: &v1alpha1.Get{
+			Resource: "foos",
+			Selector: "foo=bar",
+		},
+		want: &v1alpha1.Command{
+			Entrypoint: "kubectl",
+			Args:       []string{"get", "foos", "-l", "foo=bar", "-n", "$NAMESPACE"},
+		},
+		wantErr: false,
+	}, {
+		name: "with name and selector",
+		collector: &v1alpha1.Get{
+			Resource: "foos",
+			Name:     "foo",
+			Selector: "foo=bar",
+		},
+		wantErr: true,
+	}, {
+		name: "with namespace and selector",
+		collector: &v1alpha1.Get{
+			Resource:  "foos",
+			Namespace: "bar",
+			Selector:  "foo=bar",
+		},
+		want: &v1alpha1.Command{
+			Entrypoint: "kubectl",
+			Args:       []string{"get", "foos", "-l", "foo=bar", "-n", "bar"},
+		},
+		wantErr: false,
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := Get(tt.collector)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
