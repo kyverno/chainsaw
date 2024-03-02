4b048386657cf09170dd8cb0ad532dfd40ebf0b8
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-03-02 15:53:05 +0100

    feat: detect resource scope in describe
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/kubectl/describe.go
+++ pkg/runner/kubectl/describe.go
@@ -6,9 +6,21 @@ import (
 
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
 	"github.com/kyverno/chainsaw/pkg/client"
+	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func Describe(client client.Client, collector *v1alpha1.Describe) (*v1alpha1.Command, error) {
+	mapper := client.RESTMapper()
+	gvk, err := mapper.KindFor(schema.GroupVersionResource{Resource: collector.Resource})
+	if err != nil {
+		return nil, err
+	}
+	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return nil, err
+	}
+	clustered := mapping.Scope.Name() == meta.RESTScopeNameRoot
 	if collector == nil {
 		return nil, errors.New("collector is null")
 	}
@@ -30,12 +42,13 @@ func Describe(client client.Client, collector *v1alpha1.Describe) (*v1alpha1.Com
 	if collector.Selector != "" {
 		cmd.Args = append(cmd.Args, "-l", collector.Selector)
 	}
-	// TODO: what if cluster scoped resource ?
-	namespace := collector.Namespace
-	if collector.Namespace == "" {
-		namespace = "$NAMESPACE"
+	if !clustered {
+		namespace := collector.Namespace
+		if collector.Namespace == "" {
+			namespace = "$NAMESPACE"
+		}
+		cmd.Args = append(cmd.Args, "-n", namespace)
 	}
-	cmd.Args = append(cmd.Args, "-n", namespace)
 	if collector.ShowEvents != nil {
 		cmd.Args = append(cmd.Args, fmt.Sprintf("--show-events=%t", *collector.ShowEvents))
 	}
