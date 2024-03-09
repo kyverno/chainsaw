778a524997761fe2813ee3b25944c8d782e862b4
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-03-09 01:46:20 +0100

    feat: add env name template support in command and script (#1082)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/operations/internal/env.go
+++ pkg/runner/operations/internal/env.go
@@ -6,27 +6,22 @@ import (
 
 	"github.com/jmespath-community/go-jmespath/pkg/binding"
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
-	mutation "github.com/kyverno/chainsaw/pkg/mutate"
-	"github.com/kyverno/chainsaw/pkg/runner/functions"
-	"github.com/kyverno/kyverno-json/pkg/engine/template"
+	apibindings "github.com/kyverno/chainsaw/pkg/runner/bindings"
 )
 
 func RegisterEnvs(ctx context.Context, namespace string, bindings binding.Bindings, envs ...v1alpha1.Binding) (map[string]string, []string, error) {
 	mapOut := map[string]string{}
 	var envsOut []string
 	for _, env := range envs {
-		if err := env.CheckEnvName(); err != nil {
-			return mapOut, envsOut, err
-		}
-		patched, err := mutation.Mutate(ctx, nil, mutation.Parse(ctx, env.Value.Value), nil, bindings, template.WithFunctionCaller(functions.Caller))
+		name, value, err := apibindings.ResolveBinding(ctx, bindings, nil, env)
 		if err != nil {
 			return mapOut, envsOut, err
 		}
-		if patched, ok := patched.(string); !ok {
+		if value, ok := value.(string); !ok {
 			return mapOut, envsOut, fmt.Errorf("value must be a string (%s)", env.Name)
 		} else {
-			mapOut[env.Name] = patched
-			envsOut = append(envsOut, env.Name+"="+patched)
+			mapOut[name] = value
+			envsOut = append(envsOut, name+"="+value)
 		}
 	}
 	mapOut["NAMESPACE"] = namespace
