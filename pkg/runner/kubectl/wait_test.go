56976cac10471097f1afb26da1b95f780e85f194
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-04-03 14:38:03 +0200

    fix: wait timeout
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/kubectl/wait_test.go
+++ pkg/runner/kubectl/wait_test.go
@@ -80,7 +80,7 @@ func TestWait(t *testing.T) {
 		},
 		want: &v1alpha1.Command{
 			Entrypoint: "kubectl",
-			Args:       []string{"wait", "pods", `--for=condition=Ready="test"`, "--all", "-n", "$NAMESPACE", "--timeout=-1s"},
+			Args:       []string{"wait", "pods", `--for=condition=Ready=test`, "--all", "-n", "$NAMESPACE", "--timeout=-1s"},
 		},
 		wantErr: false,
 	}, {
@@ -98,7 +98,7 @@ func TestWait(t *testing.T) {
 		},
 		want: &v1alpha1.Command{
 			Entrypoint: "kubectl",
-			Args:       []string{"wait", "pods", `--for=condition=Ready=""`, "--all", "-n", "$NAMESPACE", "--timeout=-1s"},
+			Args:       []string{"wait", "pods", `--for=condition=Ready=`, "--all", "-n", "$NAMESPACE", "--timeout=-1s"},
 		},
 		wantErr: false,
 	}, {
@@ -170,7 +170,7 @@ func TestWait(t *testing.T) {
 			Timeout: &metav1.Duration{Duration: 120 * time.Second},
 		},
 		want: &v1alpha1.Command{
-			Timeout:    &metav1.Duration{Duration: 150 * time.Second},
+			Timeout:    &metav1.Duration{Duration: 120 * time.Second},
 			Entrypoint: "kubectl",
 			Args:       []string{"wait", "pods", "--for=condition=Ready", "--all", "-n", "$NAMESPACE", "--timeout", "2m0s"},
 		},
