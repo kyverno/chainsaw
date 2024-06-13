e1dd5cae1ac1c2736f886bd7fdd165f3e764a73f
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-06-13 10:30:31 +0200

    feat: more test v1alpha2 changes (#1533)
    * feat: more test v1alpha2 changes
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * action types
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * operation
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * catch
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    ---------
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ pkg/apis/v1alpha2/types.go
@@ -0,0 +1,22 @@
+package v1alpha2
+
+import (
+	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
+	_ "github.com/kyverno/kyverno-json/pkg/apis/policy/v1alpha1"
+)
+
+type (
+	Any             = v1alpha1.Any
+	Binding         = v1alpha1.Binding
+	Catch           = v1alpha1.CatchFinally
+	Check           = v1alpha1.Check
+	Cluster         = v1alpha1.Cluster
+	Clusters        = v1alpha1.Clusters
+	Expectation     = v1alpha1.Expectation
+	Format          = v1alpha1.Format
+	ObjectName      = v1alpha1.ObjectName
+	ObjectReference = v1alpha1.ObjectReference
+	ObjectType      = v1alpha1.ObjectType
+	Output          = v1alpha1.Output
+	Timeouts        = v1alpha1.Timeouts
+)
