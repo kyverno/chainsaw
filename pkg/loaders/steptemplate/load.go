4bfb056ab85c578e75264daf528c396d41db976f
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-07-24 00:28:25 +0200

    refactor: default loaders
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/loaders/steptemplate/load.go
+++ pkg/loaders/steptemplate/load.go
@@ -6,18 +6,17 @@ import (
 	"path/filepath"
 
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
-	internalloader "github.com/kyverno/chainsaw/pkg/internal/loader"
+	"github.com/kyverno/chainsaw/pkg/loaders"
 	yamlutils "github.com/kyverno/chainsaw/pkg/utils/yaml"
 	"github.com/kyverno/pkg/ext/resource/convert"
 	"github.com/kyverno/pkg/ext/resource/loader"
 	"github.com/kyverno/pkg/ext/yaml"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/client-go/openapi"
 )
 
 type (
 	splitter      = func([]byte) ([][]byte, error)
-	loaderFactory = func(openapi.Client) (loader.Loader, error)
+	loaderFactory = func() (loader.Loader, error)
 	converter     = func(unstructured.Unstructured) (*v1alpha1.StepTemplate, error)
 )
 
@@ -44,24 +43,17 @@ func parse(content []byte, remarshal bool, splitter splitter, loaderFactory load
 	if splitter == nil {
 		splitter = yaml.SplitDocuments
 	}
+	if loaderFactory == nil {
+		loaderFactory = loaders.DefaultLoader
+	}
 	if converter == nil {
 		converter = convert.To[v1alpha1.StepTemplate]
 	}
-	var loader loader.Loader
-	if loaderFactory != nil {
-		_loader, err := loaderFactory(internalloader.OpenApiClient)
-		if err != nil {
-			return nil, err
-		}
-		loader = _loader
-	}
-	if loader == nil {
-		if internalloader.Err != nil {
-			return nil, internalloader.Err
-		}
-		loader = internalloader.DefaultLoader
-	}
 	documents, err := splitter(content)
+	if err != nil {
+		return nil, err
+	}
+	loader, err := loaderFactory()
 	if err != nil {
 		return nil, err
 	}
