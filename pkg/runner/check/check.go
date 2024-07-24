3bf2d8c80d76a2e223e319d3aa67e4b3d14fd90e
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-07-24 14:17:14 +0200

    refactor: introduce engine package
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/check/check.go
+++ pkg/runner/check/check.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/jmespath-community/go-jmespath/pkg/binding"
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
-	"github.com/kyverno/chainsaw/pkg/runner/functions"
+	"github.com/kyverno/chainsaw/pkg/engine/functions"
 	"github.com/kyverno/kyverno-json/pkg/engine/assert"
 	"github.com/kyverno/kyverno-json/pkg/engine/template"
 	"k8s.io/apimachinery/pkg/util/validation/field"
