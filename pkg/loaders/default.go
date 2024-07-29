812e633e667638cc09b3e58a399e59d50b865d7c
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-07-29 02:21:55 +0200

    chore: add unit tests
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/loaders/default.go
+++ pkg/loaders/default.go
@@ -1,6 +1,7 @@
 package loaders
 
 import (
+	"io/fs"
 	"sync"
 
 	"github.com/kyverno/chainsaw/pkg/data"
@@ -8,12 +9,15 @@ import (
 	"sigs.k8s.io/kubectl-validate/pkg/openapiclient"
 )
 
-func defaultLoader() (loader.Loader, error) {
-	crdsFs, err := data.Crds()
+func defaultLoader(_fs func() (fs.FS, error)) (loader.Loader, error) {
+	if _fs == nil {
+		_fs = data.Crds
+	}
+	crdsFs, err := _fs()
 	if err != nil {
 		return nil, err
 	}
 	return loader.New(openapiclient.NewLocalCRDFiles(crdsFs))
 }
 
-var DefaultLoader = sync.OnceValues(defaultLoader)
+var DefaultLoader = sync.OnceValues(func() (loader.Loader, error) { return defaultLoader(nil) })
