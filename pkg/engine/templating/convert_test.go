a10b08414c9e0d397b9026cd59b6eb63a81e6f8b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-07-31 15:06:59 +0000

    chore: add unit tests (#1815)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/engine/templating/convert_test.go
+++ pkg/engine/templating/convert_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_convert(t *testing.T) {
+func Test_convertMap(t *testing.T) {
 	tests := []struct {
 		name string
 		in   any
@@ -30,6 +30,74 @@ func Test_convert(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := convertMap(tt.in)
 			assert.Equal(t, tt.out, got)
+		})
+	}
+}
+
+func Test_convertSlice(t *testing.T) {
+	tests := []struct {
+		name string
+		in   any
+		want []any
+	}{{
+		name: "nil",
+	}, {
+		name: "int",
+		in:   42,
+		want: nil,
+	}, {
+		name: "ok",
+		in: []any{
+			"foo",
+			"bar",
+		},
+		want: []any{
+			"foo",
+			"bar",
+		},
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := convertSlice(tt.in)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_convert(t *testing.T) {
+	tests := []struct {
+		name string
+		in   any
+		want any
+	}{{
+		name: "nil",
+	}, {
+		name: "int",
+		in:   42,
+		want: 42,
+	}, {
+		name: "slice",
+		in: []any{
+			"foo",
+			"bar",
+		},
+		want: []any{
+			"foo",
+			"bar",
+		},
+	}, {
+		name: "ok",
+		in: map[any]any{
+			"foo": "bar",
+		},
+		want: map[string]any{
+			"foo": "bar",
+		},
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := convert(tt.in)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
