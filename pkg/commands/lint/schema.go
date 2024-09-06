8eeda952022fa58cf92425473bef9a2d4a51ba35
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-09-06 13:14:55 +0000

    fix: lint different versions (#1924)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/commands/lint/schema.go
+++ pkg/commands/lint/schema.go
@@ -7,29 +7,10 @@ import (
 	"github.com/kyverno/chainsaw/pkg/data"
 )
 
-func getScheme(schema string) ([]byte, error) {
-	switch schema {
-	case "test":
-		return getTestSchema()
-	case "configuration":
-		return getConfigurationSchema()
-	default:
-		return nil, fmt.Errorf("unknown schema: %s", schema)
-	}
-}
-
-func getTestSchema() ([]byte, error) {
+func getScheme(kind string, version string) ([]byte, error) {
 	schemasFs, err := data.Schemas()
 	if err != nil {
 		return nil, err
 	}
-	return fs.ReadFile(schemasFs, "test-chainsaw-v1alpha1.json")
-}
-
-func getConfigurationSchema() ([]byte, error) {
-	schemasFs, err := data.Schemas()
-	if err != nil {
-		return nil, err
-	}
-	return fs.ReadFile(schemasFs, "configuration-chainsaw-v1alpha1.json")
+	return fs.ReadFile(schemasFs, fmt.Sprintf("%s-chainsaw-%s.json", kind, version))
 }
