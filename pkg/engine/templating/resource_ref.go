2835503b7294f39fac8bee0621a4917db650d9e2
Bert Baron <bertusbaron@gmail.com>
2024-11-09 15:00:03 +0100

    fix: template-functions now correctly work for name, namespace and labels
    Signed-off-by: Bert Baron <bertusbaron@gmail.com>

--- pkg/engine/templating/resource_ref.go
+++ pkg/engine/templating/resource_ref.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/kyverno/chainsaw/pkg/apis"
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
+	"github.com/kyverno/chainsaw/pkg/utils/maps"
 	"github.com/kyverno/kyverno-json/pkg/core/compilers"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
@@ -24,17 +25,27 @@ func ResourceRef(ctx context.Context, c compilers.Compilers, obj *unstructured.U
 	temp.SetKind(kind)
 	temp.SetName(obj.GetName())
 	temp.SetNamespace(obj.GetNamespace())
-	temp.SetLabels(obj.GetLabels())
+	temp.SetLabels(getStringLabels(obj))
 	template := v1alpha1.NewProjection(temp.UnstructuredContent())
 	if merged, err := TemplateAndMerge(ctx, c, temp, bindings, template); err != nil {
 		return err
 	} else {
-		temp = merged
+		obj.Object = maps.Merge(obj.Object, merged.UnstructuredContent())
 	}
-	obj.SetAPIVersion(temp.GetAPIVersion())
-	obj.SetKind(temp.GetKind())
-	obj.SetName(temp.GetName())
-	obj.SetNamespace(temp.GetNamespace())
-	obj.SetLabels(temp.GetLabels())
+	return nil
+}
+
+func getStringLabels(obj *unstructured.Unstructured) map[string]string {
+	if labels, ok, _ := unstructured.NestedFieldNoCopy(obj.UnstructuredContent(), "metadata", "labels"); ok {
+		if labelsMap, ok := labels.(map[string]interface{}); ok {
+			tempLabels := make(map[string]string)
+			for k, v := range labelsMap {
+				if stringValue, ok := v.(string); ok {
+					tempLabels[k] = stringValue
+				}
+			}
+			return tempLabels
+		}
+	}
 	return nil
 }
