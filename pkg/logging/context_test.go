8891bf31e1edd27c7b7d9eeba2ffd0f4625e0aff
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-12-11 23:09:53 +0100

    chore: add unit tests (#2220)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ pkg/logging/context_test.go
@@ -0,0 +1,51 @@
+package logging
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/fatih/color"
+	"github.com/kyverno/chainsaw/pkg/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithSink(t *testing.T) {
+	var f SinkFunc = func(string, string, Operation, Status, client.Object, *color.Color, ...fmt.Stringer) {
+	}
+	ctx := context.Background()
+	assert.NotNil(t, ctx)
+	ctx = WithSink(ctx, f)
+	assert.NotNil(t, ctx)
+}
+
+func Test_getSink(t *testing.T) {
+	var f SinkFunc = func(string, string, Operation, Status, client.Object, *color.Color, ...fmt.Stringer) {
+	}
+	ctx := context.Background()
+	assert.NotNil(t, ctx)
+	assert.Nil(t, getSink(ctx))
+	ctx = WithSink(ctx, f)
+	assert.NotNil(t, ctx)
+	assert.NotNil(t, getSink(ctx))
+}
+
+func TestWithLogger(t *testing.T) {
+	var f LoggerFunc = func(context.Context, Operation, Status, client.Object, *color.Color, ...fmt.Stringer) {
+	}
+	ctx := context.Background()
+	assert.NotNil(t, ctx)
+	ctx = WithLogger(ctx, f)
+	assert.NotNil(t, ctx)
+}
+
+func Test_getLogger(t *testing.T) {
+	var f LoggerFunc = func(context.Context, Operation, Status, client.Object, *color.Color, ...fmt.Stringer) {
+	}
+	ctx := context.Background()
+	assert.NotNil(t, ctx)
+	assert.Nil(t, getLogger(ctx))
+	ctx = WithLogger(ctx, f)
+	assert.NotNil(t, ctx)
+	assert.NotNil(t, getLogger(ctx))
+}
---
+++ pkg/logging/logger_test.go
@@ -0,0 +1,53 @@
+package logging
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/fatih/color"
+	"github.com/kyverno/chainsaw/pkg/client"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func TestLoggerFunc_Log(t *testing.T) {
+	tests := []struct {
+		name      string
+		test      string
+		step      string
+		operation Operation
+		status    Status
+		obj       client.Object
+		color     *color.Color
+		args      []fmt.Stringer
+	}{{
+		test:      "test",
+		step:      "step",
+		operation: Create,
+		status:    BeginStatus,
+		obj:       &unstructured.Unstructured{},
+		color:     color.New(color.FgBlue),
+		args:      []fmt.Stringer{Section("arg1"), Section("arg2")},
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			called := false
+			var f SinkFunc = func(test, step string, operation Operation, status Status, obj client.Object, color *color.Color, args ...fmt.Stringer) {
+				assert.Equal(t, tt.test, test)
+				assert.Equal(t, tt.step, step)
+				assert.Equal(t, tt.operation, operation)
+				assert.Equal(t, tt.status, status)
+				assert.Equal(t, tt.obj, obj)
+				assert.Equal(t, tt.color, color)
+				assert.Equal(t, tt.args, args)
+				called = true
+			}
+			ctx := context.Background()
+			ctx = WithSink(ctx, f)
+			logger := NewLogger(tt.test, tt.step)
+			logger.Log(ctx, tt.operation, tt.status, tt.obj, tt.color, tt.args...)
+			assert.True(t, called)
+		})
+	}
+}
