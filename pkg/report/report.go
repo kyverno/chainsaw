595060e3322900f9bdafa6a1fb770772fb94c3a9
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-12-12 11:48:27 +0100

    chore: add unit tests (#2224)
    * chore: add unit tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * report tests
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    ---------
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/report/report.go
+++ pkg/report/report.go
@@ -9,26 +9,27 @@ import (
 	"github.com/kyverno/chainsaw/pkg/model"
 )
 
-func Save(report *model.Report, format v1alpha2.ReportFormatType, path, name string) error {
-	getFile := func(extension string) string {
-		if filepath.Ext(name) == "" {
-			name += "." + strings.ToLower(extension)
-		}
-		filePath := name
-		if path != "" {
-			filePath = filepath.Join(path, name)
-		}
-		return filePath
+func getFile(path, name, extension string) string {
+	if filepath.Ext(name) == "" {
+		name += "." + strings.ToLower(extension)
 	}
+	filePath := name
+	if path != "" {
+		filePath = filepath.Join(path, name)
+	}
+	return filePath
+}
+
+func Save(report *model.Report, format v1alpha2.ReportFormatType, path, name string) error {
 	switch format {
 	case v1alpha2.XMLFormat, v1alpha2.JUnitTestFormat:
-		return saveJUnitTest(report, getFile("xml"))
+		return saveJUnitTest(report, getFile(path, name, "xml"))
 	case v1alpha2.JUnitStepFormat:
-		return saveJUnitStep(report, getFile("xml"))
+		return saveJUnitStep(report, getFile(path, name, "xml"))
 	case v1alpha2.JUnitOperationFormat:
-		return saveJUnitOperation(report, getFile("xml"))
+		return saveJUnitOperation(report, getFile(path, name, "xml"))
 	case v1alpha2.JSONFormat:
-		return saveJson(report, getFile("json"))
+		return saveJson(report, getFile(path, name, "json"))
 	default:
 		return fmt.Errorf("unknown report format: %s", format)
 	}
