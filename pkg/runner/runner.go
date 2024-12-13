f7956544720a5f68b1b2684345049310a9eca939
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2024-12-13 13:39:12 +0100

    refactor: runner test processor
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -5,15 +5,20 @@ import (
 	"fmt"
 	"time"
 
+	petname "github.com/dustinkirkland/golang-petname"
+	"github.com/kyverno/chainsaw/pkg/apis/v1alpha1"
 	"github.com/kyverno/chainsaw/pkg/apis/v1alpha2"
+	"github.com/kyverno/chainsaw/pkg/cleanup/cleaner"
 	"github.com/kyverno/chainsaw/pkg/discovery"
 	"github.com/kyverno/chainsaw/pkg/engine/namespacer"
 	"github.com/kyverno/chainsaw/pkg/logging"
+	"github.com/kyverno/chainsaw/pkg/model"
 	enginecontext "github.com/kyverno/chainsaw/pkg/runner/context"
 	"github.com/kyverno/chainsaw/pkg/runner/internal"
 	"github.com/kyverno/chainsaw/pkg/runner/names"
 	"github.com/kyverno/chainsaw/pkg/testing"
 	"github.com/kyverno/pkg/ext/output/color"
+	"go.uber.org/multierr"
 	"k8s.io/utils/clock"
 )
 
@@ -51,11 +56,17 @@ func (r *runner) run(ctx context.Context, m mainstart, nsOptions v1alpha2.Namesp
 		F: func(t *testing.T) {
 			t.Helper()
 			t.Parallel()
-			// configure golang context
+			// setup logger sink
 			ctx = logging.WithSink(ctx, newSink(r.clock, t.Log))
+			// setup logger
 			ctx = logging.WithLogger(ctx, logging.NewLogger(t.Name(), "@chainsaw"))
-			// setup cleaner
-			cleaner := setupCleanup(ctx, t, r.onFail, tc)
+			// setup cleanup
+			cleanup := cleaner.New(tc.Timeouts().Cleanup.Duration, nil, tc.DeletionPropagation())
+			t.Cleanup(func() {
+				if err := r.cleanup(ctx, tc, cleanup); err != nil {
+					t.Fail()
+				}
+			})
 			// setup namespace
 			var nspacer namespacer.Namespacer
 			if nsOptions.Name != "" {
@@ -64,7 +75,7 @@ func (r *runner) run(ctx context.Context, m mainstart, nsOptions v1alpha2.Namesp
 					compilers = compilers.WithDefaultCompiler(string(*nsOptions.Compiler))
 				}
 				namespaceData := namespaceData{
-					cleaner:   cleaner,
+					cleaner:   cleanup,
 					compilers: compilers,
 					name:      nsOptions.Name,
 					template:  nsOptions.Template,
@@ -82,6 +93,7 @@ func (r *runner) run(ctx context.Context, m mainstart, nsOptions v1alpha2.Namesp
 					nspacer = namespacer.New(namespace.GetName())
 				}
 			}
+			fmt.Println(nspacer)
 			// loop through tests
 			for i := range tests {
 				test := tests[i]
@@ -92,20 +104,198 @@ func (r *runner) run(ctx context.Context, m mainstart, nsOptions v1alpha2.Namesp
 					logging.Log(ctx, logging.Internal, logging.ErrorStatus, nil, color.BoldRed, logging.ErrSection(err))
 					r.onFail()
 				} else {
+					// setup logger
+					size := len("@chainsaw")
+					for i, step := range test.Test.Spec.Steps {
+						name := step.Name
+						if name == "" {
+							name = fmt.Sprintf("step-%d", i+1)
+						}
+						if size < len(name) {
+							size = len(name)
+						}
+					}
+					ctx := logging.WithLogger(ctx, logging.NewLogger(test.Test.Name, fmt.Sprintf("%-*s", size, "@chainsaw")))
+					// run test scenarios
 					testId := i + 1
+					runTest := func(t *testing.T, scenarioId int, bindings ...v1alpha1.Binding) {
+						t.Helper()
+						// setup logger sink
+						ctx := logging.WithSink(ctx, newSink(r.clock, t.Log))
+						// setup concurrency
+						if test.Test.Spec.Concurrent == nil || *test.Test.Spec.Concurrent {
+							t.Parallel()
+						}
+						// setup summary
+						defer func() {
+							if t.Skipped() {
+								tc.IncSkipped()
+							} else if t.Failed() {
+								tc.IncFailed()
+							} else {
+								tc.IncPassed()
+							}
+						}()
+						// setup reporting
+						report := &model.TestReport{
+							BasePath:   test.BasePath,
+							Name:       test.Test.Name,
+							Concurrent: test.Test.Spec.Concurrent,
+							StartTime:  time.Now(),
+						}
+						stepReport := &model.StepReport{
+							Name:      "main",
+							StartTime: time.Now(),
+						}
+						defer func() {
+							report.EndTime = time.Now()
+							report.Skipped = t.Skipped()
+							tc.Report.Add(report)
+						}()
+						// skip check
+						if test.Test.Spec.Skip != nil && *test.Test.Spec.Skip {
+							t.Skip()
+							return
+						}
+						// setup context
+						tc = tc.WithBinding("test", TestInfo{
+							Id:         testId,
+							ScenarioId: scenarioId,
+							Metadata:   test.Test.ObjectMeta,
+						})
+						tc, err := enginecontext.WithBindings(tc, bindings...)
+						if err != nil {
+							t.Fail()
+							logging.Log(ctx, logging.Internal, logging.ErrorStatus, nil, color.BoldRed, logging.ErrSection(err))
+							r.onFail()
+							return
+						}
+						contextData := contextData{
+							basePath:            test.BasePath,
+							catch:               test.Test.Spec.Catch,
+							cluster:             test.Test.Spec.Cluster,
+							clusters:            test.Test.Spec.Clusters,
+							delayBeforeCleanup:  test.Test.Spec.DelayBeforeCleanup,
+							deletionPropagation: test.Test.Spec.DeletionPropagationPolicy,
+							skipDelete:          test.Test.Spec.SkipDelete,
+							templating:          test.Test.Spec.Template,
+							terminationGrace:    test.Test.Spec.ForceTerminationGracePeriod,
+							timeouts:            test.Test.Spec.Timeouts,
+						}
+						tc, err = setupContext(tc, contextData)
+						if err != nil {
+							t.Fail()
+							logging.Log(ctx, logging.Internal, logging.ErrorStatus, nil, color.BoldRed, logging.ErrSection(err))
+							r.onFail()
+							return
+						}
+						// fail fast check
+						if tc.FailFast() && tc.Failed() > 0 {
+							t.Skip()
+							return
+						}
+						// setup cleaner
+						var cleanup cleaner.CleanerCollector
+						if !tc.SkipDelete() {
+							_cleanup := cleaner.New(tc.Timeouts().Cleanup.Duration, nil, tc.DeletionPropagation())
+							defer func() {
+								if !_cleanup.Empty() {
+									logging.Log(ctx, logging.Cleanup, logging.BeginStatus, nil, color.BoldFgCyan)
+									defer func() {
+										logging.Log(ctx, logging.Cleanup, logging.EndStatus, nil, color.BoldFgCyan)
+									}()
+									stepReport := &model.StepReport{
+										Name:      fmt.Sprintf("cleanup (%s)", stepReport.Name),
+										StartTime: time.Now(),
+									}
+									defer func() {
+										stepReport.EndTime = time.Now()
+										report.Add(stepReport)
+									}()
+									for _, err := range _cleanup.Run(ctx, stepReport) {
+										t.Fail()
+										logging.Log(ctx, logging.Cleanup, logging.ErrorStatus, nil, color.BoldRed, logging.ErrSection(err))
+										r.onFail()
+									}
+								}
+							}()
+							cleanup = _cleanup
+						}
+						// setup namespace
+						// TODO: should be part of setupContext ?
+						if test.Test.Spec.Compiler != nil {
+							tc = tc.WithDefaultCompiler(string(*test.Test.Spec.Compiler))
+						}
+						nsName := test.Test.Spec.Namespace
+						if nspacer == nil && nsName == "" {
+							nsName = fmt.Sprintf("chainsaw-%s", petname.Generate(2, "-"))
+						}
+						if nsName != "" {
+							// TODO: this may not use the right default compiler if the template is coming from the config
+							// but the default compiler is specified at the test level
+							if template := test.Test.Spec.NamespaceTemplate; template != nil && template.Value() != nil {
+								nsOptions.Template = template
+								nsOptions.Compiler = test.Test.Spec.NamespaceTemplateCompiler
+							}
+							compilers := tc.Compilers()
+							if nsOptions.Compiler != nil {
+								compilers = compilers.WithDefaultCompiler(string(*nsOptions.Compiler))
+							}
+							namespaceData := namespaceData{
+								cleaner:   cleanup,
+								compilers: compilers,
+								name:      nsName,
+								template:  nsOptions.Template,
+							}
+							nsTc, namespace, err := setupNamespace(ctx, tc, namespaceData)
+							if err != nil {
+								t.Fail()
+								logging.Log(ctx, logging.Internal, logging.ErrorStatus, nil, color.BoldRed, logging.ErrSection(err))
+								r.onFail()
+								return
+							}
+							tc = nsTc
+							if namespace != nil {
+								nspacer = namespacer.New(namespace.GetName())
+							}
+						}
+						if nspacer != nil {
+							report.Namespace = nspacer.GetNamespace()
+						} // setup bindings
+						tc, err = setupBindings(tc, test.Test.Spec.Bindings...)
+						if err != nil {
+							t.Fail()
+							logging.Log(ctx, logging.Internal, logging.ErrorStatus, nil, color.BoldRed, logging.ErrSection(err))
+							r.onFail()
+							return
+						}
+						// loop through steps
+						for i, step := range test.Test.Spec.Steps {
+							name := step.Name
+							if name == "" {
+								name = fmt.Sprintf("step-%d", i+1)
+							}
+							ctx := logging.WithLogger(ctx, logging.NewLogger(test.Test.Name, fmt.Sprintf("%-*s", size, name)))
+							info := StepInfo{
+								Id: i + 1,
+							}
+							tc := tc.WithBinding("step", info)
+							if stop := r.runStep(ctx, t, test.BasePath, nspacer, tc, step, report); stop {
+								return
+							}
+						}
+					}
 					if len(test.Test.Spec.Scenarios) == 0 {
 						t.Run(name, func(t *testing.T) {
 							t.Helper()
-							ctx = logging.WithSink(ctx, newSink(r.clock, t.Log))
-							r.runTest(ctx, t, nsOptions, nspacer, tc, test, testId, 0)
+							runTest(t, 0)
 						})
 					} else {
 						for s := range test.Test.Spec.Scenarios {
 							scenarioId := s + 1
 							t.Run(name, func(t *testing.T) {
 								t.Helper()
-								ctx = logging.WithSink(ctx, newSink(r.clock, t.Log))
-								r.runTest(ctx, t, nsOptions, nspacer, tc, test, testId, scenarioId, test.Test.Spec.Scenarios[s].Bindings...)
+								runTest(t, scenarioId, test.Test.Spec.Scenarios[s].Bindings...)
 							})
 						}
 					}
@@ -136,4 +326,22 @@ func (r *runner) onFail() {
 	if r.onFailure != nil {
 		r.onFailure()
 	}
+}
+
+func (r *runner) cleanup(ctx context.Context, tc enginecontext.TestContext, cleaner cleaner.Cleaner) error {
+	var errs []error
+	if !tc.SkipDelete() {
+		if !cleaner.Empty() {
+			logging.Log(ctx, logging.Cleanup, logging.BeginStatus, nil, color.BoldFgCyan)
+			defer func() {
+				logging.Log(ctx, logging.Cleanup, logging.EndStatus, nil, color.BoldFgCyan)
+			}()
+			for _, err := range cleaner.Run(ctx, nil) {
+				errs = append(errs, err)
+				logging.Log(ctx, logging.Cleanup, logging.ErrorStatus, nil, color.BoldRed, logging.ErrSection(err))
+				r.onFail()
+			}
+		}
+	}
+	return multierr.Combine(errs...)
 }
